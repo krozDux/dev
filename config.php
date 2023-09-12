ce0a5a57f4a52536bb855a9069889cfe8adb6570
Carlos <1971975680@undc.edu.pe>
2023-09-11 22:46:03 -0500

    C

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$usuario  = "root";
-$password = "";
+$usuario  = "u537253387_almidon";
+$password = "Carloshack1.";
 $servidor = "localhost";
-$basededatos = "almidon";
+$basededatos = "u537253387_almidon";
 $con = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
 mysqli_query($con,"SET SESSION collation_connection ='utf8_unicode_ci'");
 $db = mysqli_select_db($con, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
