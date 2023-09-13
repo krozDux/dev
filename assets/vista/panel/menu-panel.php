62b2fcbc2a5805e1266bd52c77f429d51caf672f
Carlos <1971975680@undc.edu.pe>
2023-09-12 19:19:53 -0500

    xd

--- assets/vista/panel/menu-panel.php
+++ assets/vista/panel/menu-panel.php
@@ -202,6 +202,7 @@
 											</span>
 										</div>
 
+							
 										<div class="menu-item">
 											<div class="menu-content pt-8 pb-0">
 												<span class="menu-section text-muted text-uppercase fs-8 ls-1">GESTIÓN DE AUTENTIFICACIÓN</span>
