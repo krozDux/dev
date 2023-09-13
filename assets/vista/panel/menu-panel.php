3b90c5b3cacc7b9ec0cb794d1a443318473beeb0
Carlos <1971975680@undc.edu.pe>
2023-09-12 19:26:03 -0500

    xd

--- assets/vista/panel/menu-panel.php
+++ assets/vista/panel/menu-panel.php
@@ -202,11 +202,7 @@
 											</span>
 										</div>
 
-										<div class="menu-item">
-											<div class="menu-content pt-8 pb-0">
-												<span class="menu-section text-muted text-uppercase fs-8 ls-1">GESTIÓN DE AUTENTIFICACIÓN</span>
-											</div>
-										</div>
+							
 										<div class="menu-item">
 											<div class="menu-content pt-8 pb-0">
 												<span class="menu-section text-muted text-uppercase fs-8 ls-1">GESTIÓN DE AUTENTIFICACIÓN</span>
@@ -224,7 +220,26 @@
 												<span class="menu-title">Usuarios</span>
 											</span>
 										</div>
-										
+										<div class="menu-item">
+											<div class="menu-content pt-8 pb-0">
+												<span class="menu-section text-muted text-uppercase fs-8 ls-1">Layout</span>
+											</div>
+										</div>
+										<div class="menu-item">
+											<a class="menu-link" href="https://preview.keenthemes.com/metronic8/demo14/layout-builder.html" title="Build your layout and export HTML for server side integration" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+												<span class="menu-icon">
+													<!--begin::Svg Icon | path: icons/duotune/general/gen019.svg-->
+													<span class="svg-icon svg-icon-2">
+														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+															<path d="M17.5 11H6.5C4 11 2 9 2 6.5C2 4 4 2 6.5 2H17.5C20 2 22 4 22 6.5C22 9 20 11 17.5 11ZM15 6.5C15 7.9 16.1 9 17.5 9C18.9 9 20 7.9 20 6.5C20 5.1 18.9 4 17.5 4C16.1 4 15 5.1 15 6.5Z" fill="currentColor" />
+															<path opacity="0.3" d="M17.5 22H6.5C4 22 2 20 2 17.5C2 15 4 13 6.5 13H17.5C20 13 22 15 22 17.5C22 20 20 22 17.5 22ZM4 17.5C4 18.9 5.1 20 6.5 20C7.9 20 9 18.9 9 17.5C9 16.1 7.9 15 6.5 15C5.1 15 4 16.1 4 17.5Z" fill="currentColor" />
+														</svg>
+													</span>
+													<!--end::Svg Icon-->
+												</span>
+												<span class="menu-title">Layout Builder</span>
+											</a>
+										</div>
 									</div>
 									<?php } ?>
 								</div>
