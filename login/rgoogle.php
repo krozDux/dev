eedf6110b0dbcb92bb565a19a9214c27f1efde77
Carlos <1971975680@undc.edu.pe>
2023-09-14 18:54:00 -0500

    registro

--- login/rgoogle.php
+++ login/rgoogle.php
@@ -58,10 +58,10 @@ $resultado_email = mysqli_query($con, $consulta_email);
 
 <!DOCTYPE html>
 <html lang="es">
-	<?php include_once '../assets/vista/login/head-login.php'?>
+	<?php include_once '../assets/vista/registro/head-registro.php'?>
 	<body id="kt_body" class="bg-body">
-	<?php include_once '../assets/vista/login/body-login.php'?>
+	<?php include_once '../assets/vista/registro/body-registro.php'?>
 		<script src="assets/plugins/global/plugins.bundle.js"></script>
 		<script src="assets/js/scripts.bundle.js"></script>
 	</body>
-	</html>
+</html>
