02c0c39c7f62b3d157a934b7f87fe90d77e6a289
Carlos <1971975680@undc.edu.pe>
2023-09-15 01:29:59 -0500

    xD

--- assets/controlador/credenciales.php
+++ assets/controlador/credenciales.php
@@ -1,5 +1,13 @@
 <?php
 include('../config.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/src/SMTP.php';
+
+require 'vendor/autoload.php';
 if (!empty($_POST['btneditar'])) {
     if (!empty($_POST['email'])) {
         $email = $_POST['email'];
@@ -7,7 +15,6 @@ if (!empty($_POST['btneditar'])) {
                 $consulta_email = "SELECT * FROM usuarios WHERE email = '$email'";
                 $resultado_email = mysqli_query($con, $consulta_email);
                 if (mysqli_num_rows($resultado_email) == 0) {
-
                     // El correo no está registrado, procede a insertar el nuevo usuario
                     echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                     <div class="toast-header">
@@ -20,10 +27,45 @@ if (!empty($_POST['btneditar'])) {
                     </div>
                     </div>';
                 } else {
-                    $hashedPassword = hash('sha256', $password);
-                        $consulta = "INSERT usuarios (`email`,`pass`,`nombres`,`apellidos`,`imagen`,`rol`) VALUES ('$email','$hashedPassword','$nombres','$apellidos','blank.png','usuario')";
-                        $resultado = mysqli_query($con, $consulta);
-                        header("location: index.php");
+                    $token = bin2hex(random_bytes(16));
+                    $timestamp = date('Y-m-d H:i:s');
+                    $consulta = "INSERT INTO solicitudes_reseteo (email, token, created_at) VALUES ('$email', '$token', '$timestamp')";
+                    mysqli_query($con, $consulta);
+                    $mail = new PHPMailer(true);
+                    try {
+                        //Server settings
+                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                        $mail->isSMTP();                                            //Send using SMTP
+                        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                        $mail->Username   = 'user@example.com';                     //SMTP username
+                        $mail->Password   = 'secret';                               //SMTP password
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                        //Recipients
+                        $mail->setFrom('from@example.com', 'Mailer');
+                        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+                        $mail->addAddress('ellen@example.com');               //Name is optional
+                        $mail->addReplyTo('info@example.com', 'Information');
+                        $mail->addCC('cc@example.com');
+                        $mail->addBCC('bcc@example.com');
+
+                        //Attachments
+                        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+                        //Content
+                        $mail->isHTML(true);                                  //Set email format to HTML
+                        $mail->Subject = 'Here is the subject';
+                        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                        $mail->send();
+                        echo 'Message has been sent';
+                    } catch (Exception $e) {
+                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    }
                 }
         }
     }
