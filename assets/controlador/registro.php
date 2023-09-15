2a6a30f31519551d02547d3389b3f5f082a34411
Carlos <1971975680@undc.edu.pe>
2023-09-14 21:52:53 -0500

    probar

--- assets/controlador/registro.php
+++ assets/controlador/registro.php
@@ -1,15 +1,18 @@
 <?php
 include('../config.php');
+
 if (!empty($_POST['btnregistrar'])) {
-    if (!empty($_POST['email']) and !empty($_POST['password']) and !empty($_POST['nombres']) and !empty($_POST['apellidos'])) {
+    if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['nombres']) && !empty($_POST['apellidos'])) {
         $email = $_POST['email'];
         $imagen = $_FILES['imagen']['name'];
+        $imagen_tmp = $_FILES['imagen']['tmp_name'];
         $password = $_POST['password'];
         $password2 = $_POST['password2'];
         $nombres = $_POST['nombres'];
         $apellidos = $_POST['apellidos'];
         $nombres = strtoupper($nombres);
         $apellidos = strtoupper($apellidos);
+
         // Verificar el formato del correo electrónico
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Verificar que el nombre y apellido no contengan caracteres especiales
@@ -21,15 +24,26 @@ if (!empty($_POST['btnregistrar'])) {
                         // Comprobar si el correo ya está registrado
                         $consulta_email = "SELECT * FROM usuarios WHERE email = '$email'";
                         $resultado_email = mysqli_query($con, $consulta_email);
+
                         if (mysqli_num_rows($resultado_email) == 0) {
-                            if (isset($imagen) and $imagen!=""){
-                                    $tipo = $_FILES['imagen']['type'];
-                                    $temp = $_FILES['imagen']['tmp_name'];
-                                    
-                                    // $extension = pathinfo($imagen,PATHINFO_EXTENSION);
-                                    // $imagenid = $rename.'.'.$extension;
-                                if (!((strpos($tipo, 'jpeg')) or (strpos($tipo, 'jpg')) or (strpos($tipo, 'png')))){
-                                    echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+                            // Verificar el tipo de archivo (solo PNG o JPG permitidos)
+                            $extension = pathinfo($imagen, PATHINFO_EXTENSION);
+                            $extensiones_permitidas = array('png', 'jpg', 'jpeg');
+
+                            if (in_array(strtolower($extension), $extensiones_permitidas)) {
+                                // Renombrar la imagen con la ID del usuario
+                                $id_usuario = mysqli_insert_id($con);
+                                $nuevo_nombre_imagen = $id_usuario . '.' . $extension;
+                                $ruta_imagen = '../assets/media/avatars/' . $nuevo_nombre_imagen;
+                                move_uploaded_file($imagen_tmp, $ruta_imagen);
+
+                                $hashedPassword = hash('sha256', $password);
+                                $consulta = "INSERT usuarios (`email`,`pass`,`nombres`,`apellidos`,`imagen`,`rol`,`metodoLogin`,`estado`) VALUES ('$email','$hashedPassword','$nombres','$apellidos','$nuevo_nombre_imagen','invitado','email','1')";
+                                $resultado = mysqli_query($con, $consulta);
+                                header("location: index.php");
+                            } else {
+                                // Mostrar un mensaje de error si el tipo de archivo no es válido
+                                echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                                     <div class="toast-header">
                                         <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                                         <strong class="me-auto">Alerta</strong>
@@ -39,90 +53,78 @@ if (!empty($_POST['btnregistrar'])) {
                                         Solo se permiten archivos con extensión jpeg, jpg o png.
                                     </div>
                                 </div>';
-                                }else {
-                                    move_uploaded_file($temp, '../assets/media/avatars/'.$imagen);
-                                    $hashedPassword = hash('sha256', $password);
-                                    $consulta = "INSERT usuarios (`email`,`pass`,`nombres`,`apellidos`,`imagen`,`rol`,`metodoLogin`,`estado`) VALUES ('$email','$hashedPassword','$nombres','$apellidos','$imagen','invitado','email','1')";
-                                    $resultado = mysqli_query($con, $consulta);
-                                    header("location: index.php");
-                                }
-                            }else {
-                                $hashedPassword = hash('sha256', $password);
-                                $consulta = "INSERT usuarios (`email`,`pass`,`nombres`,`apellidos`,`imagen`,`rol`,`metodoLogin`,`estado`) VALUES ('$email','$hashedPassword','$nombres','$apellidos','blank.png','invitado','email','1')";
-                                $resultado = mysqli_query($con, $consulta);
-                                header("location: index.php");
                             }
                         } else {
                             echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+                                <div class="toast-header">
+                                    <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
+                                    <strong class="me-auto">Alerta</strong>
+                                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                                </div>
+                                <div class="toast-body">
+                                    El correo electrónico ya está registrado. Por favor, elija otro correo.
+                                </div>
+                            </div>';
+                        }
+                    } else {
+                        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                             <div class="toast-header">
                                 <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                                 <strong class="me-auto">Alerta</strong>
                                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                             </div>
                             <div class="toast-body">
-                                El correo electrónico ya está registrado. Por favor, elija otro correo.
+                                Las contraseñas no coinciden.
                             </div>
                         </div>';
-                        }
-                    } else {
-                        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+                    }
+                } else {
+                    echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                         <div class="toast-header">
                             <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                             <strong class="me-auto">Alerta</strong>
                             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                         </div>
                         <div class="toast-body">
-                            Las contraseñas no coinciden.
+                            La contraseña debe tener al menos una letra mayúscula, un símbolo y un mínimo de 8 caracteres.
                         </div>
                     </div>';
-                    }
-                } else {
-                    echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+                }
+            } else {
+                echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                     <div class="toast-header">
                         <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                         <strong class="me-auto">Alerta</strong>
                         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                     </div>
                     <div class="toast-body">
-                        La contraseña debe tener al menos una letra mayúscula, un símbolo y un mínimo de 8 caracteres.
+                        El nombre y apellido solo deben contener letras y espacios.
                     </div>
                 </div>';
-                }
-            } else {
-                echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+            }
+        } else {
+            echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="toast-header">
                     <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                     <strong class="me-auto">Alerta</strong>
                     <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
                 <div class="toast-body">
-                    El nombre y apellido solo deben contener letras y espacios.
+                    El formato del correo electrónico no es válido.
                 </div>
             </div>';
-            }
-        } else {
-            echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+        }
+    } else {
+        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
                 <strong class="me-auto">Alerta</strong>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                El formato del correo electrónico no es válido.
+                Por favor, completa todos los campos.
             </div>
         </div>';
-        }
-    } else {
-        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
-            <strong class="me-auto">Alerta</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            Por favor, completa todos los campos.
-        </div>
-    </div>';
     }
 }
 ?>
