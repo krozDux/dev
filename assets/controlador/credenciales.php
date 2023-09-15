137ba1af4b7c339266c79e08b61471c3dc269803
Carlos <1971975680@undc.edu.pe>
2023-09-15 02:02:10 -0500

    xd

--- assets/controlador/credenciales.php
+++ assets/controlador/credenciales.php
@@ -10,33 +10,6 @@ require 'vendor/autoload.php';
 
 if (!empty($_POST['btncredenciales'])) {
     if (!empty($_POST['email'])) {
-        $email = $_POST['email'];
-        $mail = new PHPMailer(true);
-                    try {
-                        //Server settings
-                        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                        $mail->isSMTP();                                            //Send using SMTP
-                        $mail->Host       = 'smtp.tital.email';                     //Set the SMTP server to send through
-                        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'carlos@nsconcepcion.edu.pe';                     //SMTP username
-                        $mail->Password   = 'Carloshack1.';                               //SMTP password
-                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-                        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                        //Recipients
-                        $mail->setFrom('carlos@nsconcepcion.edu.pe', 'Sistema WEB');
-                        $mail->addAddress('krozfix@gmail.com', 'Carlos Andres');     //Add a recipient
-                        //Content
-                        $mail->isHTML(true);                                  //Set email format to HTML
-                        $mail->Subject = 'Titulo';
-                        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                        $mail->send();
-                        echo 'Message has been sent';
-                    } catch (Exception $e) {
-                        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                    }
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $consulta_email = "SELECT * FROM usuarios WHERE email = '$email'";
                 $resultado_email = mysqli_query($con, $consulta_email);
