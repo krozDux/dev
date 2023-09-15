c75105362c87bb7ad0ca6d6d01612e4143655bbb
Carlos <1971975680@undc.edu.pe>
2023-09-14 21:18:07 -0500

    probar

--- assets/controlador/login.php
+++ assets/controlador/login.php
@@ -1,23 +1,46 @@
 <?php
 include('../config.php');
-if (!empty($_POST['btningresar'])){
-    if (!empty($_POST['email']) and !empty($_POST['password']) ){
+if (!empty($_POST['btningresar'])) {
+    if (!empty($_POST['email']) and !empty($_POST['password'])) {
         $email = $_POST['email'];
         $password = $_POST['password'];
         $hashedPassword = hash('sha256', $password);
+
         // Utilizar una consulta preparada
-        $sql = "SELECT * FROM `usuarios` WHERE email = ? AND pass = ? AND estado = 1";
+        $sql = "SELECT * FROM `usuarios` WHERE email = ? AND pass = ?";
         $stmt = mysqli_prepare($con, $sql);
         mysqli_stmt_bind_param($stmt, "ss", $email, $hashedPassword);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_store_result($stmt);
         $total = mysqli_stmt_num_rows($stmt);
- 
-        if ($total >= 1){
-            session_start();
-            $_SESSION['email'] = $email;
-            header("location:../panel/index.php");
-        } else{
+
+        if ($total >= 1) {
+            // Verificar el estado de la cuenta
+            $sql = "SELECT estado FROM `usuarios` WHERE email = ?";
+            $stmt = mysqli_prepare($con, $sql);
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $estado);
+            mysqli_stmt_fetch($stmt);
+
+            if ($estado == 2) {
+                echo ' <div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
+                    <div class="toast-header">
+                        <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
+                        <strong class="me-auto">Alerta</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                    <div class="toast-body">
+                        Su cuenta está desactivada.
+                    </div>
+                </div> ';
+            } else {
+                // Iniciar sesión
+                session_start();
+                $_SESSION['email'] = $email;
+                header("location:../panel/index.php");
+            }
+        } else {
             echo ' <div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="toast-header">
                     <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
@@ -30,8 +53,7 @@ if (!empty($_POST['btningresar'])){
             </div> ';
         }
         mysqli_stmt_close($stmt);
-        
-    }else{
+    } else {
         echo ' <div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
                 <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
