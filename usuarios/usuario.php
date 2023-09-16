d169a86a4075d2452739d6a2ddff8165ac7528ec
Carlos <1971975680@undc.edu.pe>
2023-09-15 23:48:52 -0500

    ga

---
+++ usuarios/usuario.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+	<?php include_once '../assets/controlador/sesion.php'?>
+	<?php include_once '../assets/vista/usuarios/head-usuarios.php'?>
+	<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed">
+		<?php include_once '../assets/vista/usuarios/body-usuario.php'?>
+		<script>var hostUrl = "assets/";</script>
+		<script src="assets/plugins/global/plugins.bundle.js"></script>
+		<script src="assets/js/scripts.bundle.js"></script>
+		<script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
+	</body>
+</html>
