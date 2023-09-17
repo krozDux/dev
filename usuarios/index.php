0c9625b5737dc82b1d37edd0e055e925c453ced4
Carlos <1971975680@undc.edu.pe>
2023-09-17 05:06:24 -0500

    xd

--- usuarios/index.php
+++ usuarios/index.php
@@ -14,7 +14,7 @@
 		<script>
 		$(document).ready(function () {
 			$('#kt_table_users').DataTable({
-				dom: 'fBrtip',
+				dom: 'Plfrtip';
 				buttons: [
 					{
 						text: '<span class="svg-icon svg-icon-2">'+
