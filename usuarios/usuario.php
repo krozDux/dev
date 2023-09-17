a0f8b4275310da121d2fe20f952be0be315da160
Carlos <1971975680@undc.edu.pe>
2023-09-17 04:19:03 -0500

    ga

--- usuarios/usuario.php
+++ usuarios/usuario.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<?php  if ($session_rol = "admin") { ?>
+<?php  if ($session_rol == "admin") { ?>
 <html lang="es">
 	<?php include_once '../assets/controlador/sesion.php'?>
 	<?php include_once '../assets/vista/usuarios/head-usuarios.php'?>
