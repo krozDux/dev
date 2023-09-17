49c09054edb657021c55ebff616f9b838a5ea7bb
Carlos <1971975680@undc.edu.pe>
2023-09-17 02:45:07 -0500

    waaa

--- assets/controlador/usuarios.php
+++ assets/controlador/usuarios.php
@@ -25,7 +25,7 @@ if (!empty($_POST['btnreset'])) {
 }}
 
 if (!empty($_POST['btneditar'])) {
-    if (!empty($_POST['editId']) and !empty($_POST['editNombre']) and !empty($_POST['editEmail'])) {
+    if (!empty($_POST['editId']) ) {
         $editId = $_POST['editId'];
         $editRol = $_POST['editRol'];
         $editNumero = $_POST['editNumero'];
