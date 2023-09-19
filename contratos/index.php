b07b31676b370fceb3a36282e7008828e1275c40
Carlos <1971975680@undc.edu.pe>
2023-09-18 23:41:48 -0500

    wa

--- contratos/index.php
+++ contratos/index.php
@@ -16,12 +16,10 @@
 			$('#kt_table_users').DataTable({
 			
 				dom: 'fBrtip',
-			
     			responsive: true,
 				columnDefs: [
 					{ responsivePriority: 1, targets: 0 },
-					{ responsivePriority: 2, targets: 1 },
-					{ responsivePriority: 3, targets: 2 }
+					{ responsivePriority: 2, targets: -1 },
 				],
 				buttons: [
 					{
