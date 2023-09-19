00d08fa1962fae0cfd58f9feb17426cdab000b97
Carlos <1971975680@undc.edu.pe>
2023-09-19 00:49:29 -0500

    waaw

--- assets/vista/clientes/tabla-clientes.php
+++ assets/vista/clientes/tabla-clientes.php
@@ -50,13 +50,13 @@ $query1 = mysqli_query($con, $sql1);
 																data-direccion="<?php echo $dataUsuario1['direccion']; ?>"
 																data-numero="<?php echo $dataUsuario1['numero']; ?>"
 																data-imagen="<?php echo $dataUsuario1['imagen']; ?>"
-																><span class="bi bi-pencil-fill fs-7"></buttton>
+																><span class="bi bi-pencil-fill fs-7 opacity-50"></buttton>
 																<buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm del-usuario" 
 																data-id="<?php echo $dataUsuario1['id']; ?>"
 																data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
 																data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
 																data-rol="<?php echo $dataUsuario1['rol']; ?>"
-																><span class="svg-icon svg-icon-3">
+																><span class="svg-icon svg-icon-3 ">
 																	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 																		<path d="M5 9C5 8.44772 5.44772 8 6 8H18C18.5523 8 19 8.44772 19 9V18C19 19.6569 17.6569 21 16 21H8C6.34315 21 5 19.6569 5 18V9Z" fill="currentColor"></path>
 																		<path opacity="0.5" d="M5 5C5 4.44772 5.44772 4 6 4H18C18.5523 4 19 4.44772 19 5V5C19 5.55228 18.5523 6 18 6H6C5.44772 6 5 5.55228 5 5V5Z" fill="currentColor"></path>
