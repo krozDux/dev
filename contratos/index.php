f0bdd25efae3cec2bc9850c46f5ae579e1058a22
Carlos <1971975680@undc.edu.pe>
2023-09-18 23:58:04 -0500

    wa

--- contratos/index.php
+++ contratos/index.php
@@ -1,8 +1,7 @@
+<?php  if ($session_rol = "admin") { ?>
 <!DOCTYPE html>
-
 <html lang="es">
 	<?php include_once '../assets/controlador/sesion.php'?>
-	<?php  if ($session_rol = "admin") { ?>
 	<?php include_once '../assets/vista/contratos/head-contratos.php'?>
 	<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed">
 		<?php include_once '../assets/vista/contratos/body-contratos.php'?>
