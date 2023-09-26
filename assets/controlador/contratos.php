bbf7e10b44ccd4dbf23c679c147d449e590bf9f8
Carlos <1971975680@undc.edu.pe>
2023-09-26 12:50:40 -0500

    asd

--- assets/controlador/contratos.php
+++ assets/controlador/contratos.php
@@ -13,8 +13,8 @@ if (!empty($_POST['btneliminar'])) {
 if (!empty($_POST['btnreg'])) {
     if (!empty($_POST['regId']) and !empty($_POST['regFechaInicio']) and !empty($_POST['regFechaFin'])) {
         $regId = $_POST['regId'];
-        $regFechaInicio = $_POST['FechaInicio'];
-        $regFechaFin = $_POST['FechaFin'];
+        $regFechaInicio = date($_POST['FechaInicio']);
+        $regFechaFin = date($_POST['FechaFin']);
         
             $consulta5 = "UPDATE `contratos` SET `fechaInicio`='$regFechaInicio' , `FechaFin`='$regFechaFin' where id='$regId'";
             $resultado5 = mysqli_query($con, $consulta5);
