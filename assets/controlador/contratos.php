0c7d73a3d696d11641caf17b52c846e2ae70e176
Carlos <1971975680@undc.edu.pe>
2023-09-26 17:18:50 -0500

    wa

--- assets/controlador/contratos.php
+++ assets/controlador/contratos.php
@@ -18,17 +18,9 @@ if (!empty($_POST['btnreg'])) {
         
             $consulta5 = "UPDATE `contratos` SET `fechaInicio`=date($regFechaInicio) , `FechaFin`='$regFechaFin' where id='$regId'";
             $resultado5 = mysqli_query($con, $consulta5);
-            echo '<div class="toast show position-fixed bottom-0 end-0 p-2 " role="alert" aria-live="assertive" aria-atomic="true">
-                    <div class="toast-header">
-                        <i class="ki-duotone ki-abstract-39 fs-2 text-primary "><span class="path1"></span><span class="path2"></span></i>
-                        <strong class="me-auto">Alerta</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">'+
-                    $regFechaInicio+
-                    '</div>
-                    </div>';
-            // header("location: index.php");
+            header("location: index.php");
+            echo $regFechaInicio;
+            echo $regFechaFin;
     }
 }
 ?>
