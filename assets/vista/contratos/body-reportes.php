61c813b5c8d603e986dabdd5a16c68b17b4cb501
Carlos <1971975680@undc.edu.pe>
2023-09-29 02:33:52 -0500

    wa

--- assets/vista/contratos/body-reportes.php
+++ assets/vista/contratos/body-reportes.php
@@ -65,7 +65,7 @@ $query14 = mysqli_query($con, $sql14);
 	                                    <div class="card-header border-0 pt-5">
 	                                        <h3 class="card-title align-items-start flex-column">
 	                                            <span class="card-label fw-bolder fs-3 mb-1">Inicialización de contratos</span>
-	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de inicialización de contratos iniciados por mes</span>
+	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de inicialización de contratos por mes</span>
 	                                        </h3>
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
@@ -95,7 +95,7 @@ $query14 = mysqli_query($con, $sql14);
 	                                    <div class="card-header border-0 pt-5">
 	                                        <h3 class="card-title align-items-start flex-column">
 	                                            <span class="card-label fw-bolder fs-3 mb-1">Inicialización de contratos</span>
-	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de inicialización de contratos iniciados por mes y rol</span>
+	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de inicialización de contratos por mes y rol</span>
 	                                        </h3>
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
