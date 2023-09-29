7f8fe6333a398b1d1efb09b6df536be9e5db21a3
Carlos <1971975680@undc.edu.pe>
2023-09-29 01:29:41 -0500

    wa

--- contratos/reportes.php
+++ contratos/reportes.php
@@ -29,11 +29,11 @@
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
+          ['Mes', 'Sales', 'Expenses'],
+          ['Enero', 1000, 400],
+          ['Febrero', 1170, 460],
+          ['Marzo', 660, 1120],
+          ['Abril', 1030, 540]
         ]);
 
         var options = {
