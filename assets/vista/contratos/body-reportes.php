dc3d6a033e9b63a23100f742e7d38674ad529855
Carlos <1971975680@undc.edu.pe>
2023-09-29 02:09:27 -0500

    wa

--- assets/vista/contratos/body-reportes.php
+++ assets/vista/contratos/body-reportes.php
@@ -64,8 +64,8 @@ $query14 = mysqli_query($con, $sql14);
 	                                <div class="card h-xl-100 pb-5">
 	                                    <div class="card-header border-0 pt-5">
 	                                        <h3 class="card-title align-items-start flex-column">
-	                                            <span class="card-label fw-bolder fs-3 mb-1">Files</span>
-	                                            <span class="text-muted mt-1 fw-bold fs-7">Over 100 pending files</span>
+	                                            <span class="card-label fw-bolder fs-3 mb-1">Contratos</span>
+	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de contratos inciados por mes</span>
 	                                        </h3>
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
@@ -79,8 +79,8 @@ $query14 = mysqli_query($con, $sql14);
                                     <div class="card h-xl-100 pb-5">
 	                                    <div class="card-header border-0 pt-5">
 	                                        <h3 class="card-title align-items-start flex-column">
-	                                            <span class="card-label fw-bolder fs-3 mb-1">Files</span>
-	                                            <span class="text-muted mt-1 fw-bold fs-7">Over 100 pending files</span>
+	                                            <span class="card-label fw-bolder fs-3 mb-1">Contratos</span>
+	                                            <span class="text-muted mt-1 fw-bold fs-7">Cantidad de contratos finalizados por mes</span>
 	                                        </h3>
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
