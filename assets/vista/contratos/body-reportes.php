38d9cc296f98414941cd0f74b30fd3f6dd80985d
Carlos <1971975680@undc.edu.pe>
2023-09-29 00:43:28 -0500

    wa

--- assets/vista/contratos/body-reportes.php
+++ assets/vista/contratos/body-reportes.php
@@ -71,9 +71,8 @@ $query14 = mysqli_query($con, $sql14);
 	                                        </h3>
 	                                    </div>
 	                                    <div class="card-body py-3">
-	                                        <div class="table-responsive">
-                                                <div id="chart_div">
-                                                </div>
+	                                        <div class="table-responsive" style="max-width: 800px;">
+                                                <div id="chart_div"></div>
 	                                        </div>
 	                                    </div>
 	                                </div>
