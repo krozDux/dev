83d4c032f2c2df044d9971ed9a4a18fa2bafd61a
Carlos <1971975680@undc.edu.pe>
2023-09-28 21:56:37 -0500

    wa

--- assets/vista/contratos/body-contratos.php
+++ assets/vista/contratos/body-contratos.php
@@ -149,6 +149,78 @@
     </div>
 </div>
 
+<div class="modal fade" id="kt_modal_reg_user" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <div class="modal-content">
+            <div class="modal-header" id="kt_modal_add_user_header">
+                <h2 class="fw-bolder">Ver contrato</h2>
+                <div class="btn btn-icon btn-sm btn-active-icon-primary modal-close" data-kt-users-modal-action="close">
+                    <span class="svg-icon svg-icon-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div class="modal-body scroll-y mx-5 mx-xl-15 my-4">
+                <form class="form" method="POST" enctype="multipart/form-data">
+                    <div class="d-flex flex-column scroll-y me-n7 pe-7" id="kt_modal_add_user_scroll"
+                        data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}"
+                        data-kt-scroll-max-height="auto" data-kt-scroll-dependencies="#kt_modal_add_user_header"
+                        data-kt-scroll-wrappers="#kt_modal_add_user_scroll" data-kt-scroll-offset="300px">
+                        <div class="row fv-row mb-4">
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Nombres completos</label>
+                                <input type="text" name="regNombre" class="form-control form-control-solid mb-3 mb-lg-0" id="regNombres" disabled />
+                            </div>
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Rol</label>
+                                <input type="text" name="regRol"
+                                    class="form-control form-control-solid mb-3 mb-lg-0 col-m-2" id="regRol" disabled/>
+                            </div>
+                        </div>
+                        <div class="fv-row mb-4">
+                            <label class="fw-bold fs-6 mb-2">Email</label>
+                            <input type="email" name="regEmail" class="form-control form-control-solid mb-3 mb-lg-0"
+                                id="regEmail" disabled />
+                        </div>
+                        <div class="fv-row mb-4">
+                            <label class="fw-bold fs-6 mb-2">Número</label>
+                            <input type="text" name="regNumero"
+                                class="form-control form-control-solid mb-3 mb-lg-0 col-m-2" id="regNumero" disabled />
+                        </div>
+                        <div class="row fv-row mb-4">
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Fecha Inicio</label>
+                                <input type="date" name="regFechaInicio"
+                                    class="form-control form-control-solid mb-3 mb-lg-0 col-m-2" id="regFechaInicio" disabled/>
+                            </div>
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Fecha Fin</label>
+                                <input type="date" name="regFechaFin"
+                                    class="form-control form-control-solid mb-3 mb-lg-0 col-m-2" id="regFechaFin" disabled/>
+                            </div>
+                        </div>
+                        <div class="row fv-row mb-4">
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Observación</label>
+                                <input type="text" name="regObservacion" class="form-control form-control-solid mb-3 mb-lg-0" id="regObservacion" disabled/>
+                            </div>
+                            <div class="col-xl-6">
+                                <label class="required fw-bold fs-6 mb-2">Recomendación</label>
+                                <input type="text" name="regRecomendacion" class="form-control form-control-solid mb-3 mb-lg-0 col-m-2" id="regRecomendacion" disabled/>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" id="kt_modal_remove_user" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered mw-650px">
         <div class="modal-content">
--- assets/vista/contratos/tabla-contratos.php
+++ assets/vista/contratos/tabla-contratos.php
@@ -77,7 +77,20 @@ $query1 = mysqli_query($con, $sql1);
             <td><?php echo $dataUsuario1['observacion']; ?></td>
             <td><?php echo $dataUsuario1['recomendacion']; ?></td>
             <?php if ($dataUsuario1['fechaFin'] != "" and $dataUsuario1['fechaInicio'] != "") { ?>
-            <td> <div class="badge badge-success fw-bolder">Con contrato</div> </td>
+                <td>
+                <buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm ver-contrato"
+                    data-id="<?php echo $dataUsuario1['id']; ?>" data-email="<?php echo $dataUsuario1['email']; ?>"
+                    data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
+                    data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
+                    data-direccion="<?php echo $dataUsuario1['direccion']; ?>"
+                    data-rol="<?php echo ucfirst($dataUsuario1['rol']); ?>"
+                    data-numero="<?php echo $dataUsuario1['numero']; ?>"
+                    data-observacion="<?php echo $dataUsuario1['observacion']; ?>"
+                    data-recomendacion="<?php echo $dataUsuario1['recomendacion']; ?>"
+                    data-fechainicio="<?php echo $dataUsuario1['fechaInicio']; ?>"
+                    data-fechafin="<?php echo $dataUsuario1['fechaFin']; ?>"><span
+                        class="bi bi-eye-fill fs-7 opacity-50"></buttton>
+            </td>
             <?php } else { ?>
                 <td>
                 <buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm reg-contrato"
