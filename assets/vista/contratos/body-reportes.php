d71d927c08da03c8029012c80b9c31cdce830760
Carlos <1971975680@undc.edu.pe>
2023-09-29 00:35:35 -0500

    wa

--- assets/vista/contratos/body-reportes.php
+++ assets/vista/contratos/body-reportes.php
@@ -69,126 +69,10 @@ $query14 = mysqli_query($con, $sql14);
 	                                            <span class="card-label fw-bolder fs-3 mb-1">Files</span>
 	                                            <span class="text-muted mt-1 fw-bold fs-7">Over 100 pending files</span>
 	                                        </h3>
-	                                        <div class="card-toolbar">
-	                                            <!--begin::Menu-->
-	                                            <button type="button"
-	                                                class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary"
-	                                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-	                                                <!--begin::Svg Icon | path: icons/duotune/general/gen024.svg-->
-	                                                <span class="svg-icon svg-icon-2">
-	                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
-	                                                        viewBox="0 0 24 24">
-	                                                        <g stroke="none" stroke-width="1" fill="none"
-	                                                            fill-rule="evenodd">
-	                                                            <rect x="5" y="5" width="5" height="5" rx="1"
-	                                                                fill="currentColor" />
-	                                                            <rect x="14" y="5" width="5" height="5" rx="1"
-	                                                                fill="currentColor" opacity="0.3" />
-	                                                            <rect x="5" y="14" width="5" height="5" rx="1"
-	                                                                fill="currentColor" opacity="0.3" />
-	                                                            <rect x="14" y="14" width="5" height="5" rx="1"
-	                                                                fill="currentColor" opacity="0.3" />
-	                                                        </g>
-	                                                    </svg>
-	                                                </span>
-	                                                <!--end::Svg Icon-->
-	                                            </button>
-	                                            <!--begin::Menu 3-->
-	                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-200px py-3"
-	                                                data-kt-menu="true">
-	                                                <!--begin::Heading-->
-	                                                <div class="menu-item px-3">
-	                                                    <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">
-	                                                        Payments</div>
-	                                                </div>
-	                                                <!--end::Heading-->
-	                                                <!--begin::Menu item-->
-	                                                <div class="menu-item px-3">
-	                                                    <a href="#" class="menu-link px-3">Create Invoice</a>
-	                                                </div>
-	                                                <!--end::Menu item-->
-	                                                <!--begin::Menu item-->
-	                                                <div class="menu-item px-3">
-	                                                    <a href="#" class="menu-link flex-stack px-3">Create Payment
-	                                                        <i class="fas fa-exclamation-circle ms-2 fs-7"
-	                                                            data-bs-toggle="tooltip"
-	                                                            title="Specify a target name for future usage and reference"></i></a>
-	                                                </div>
-	                                                <!--end::Menu item-->
-	                                                <!--begin::Menu item-->
-	                                                <div class="menu-item px-3">
-	                                                    <a href="#" class="menu-link px-3">Generate Bill</a>
-	                                                </div>
-	                                                <!--end::Menu item-->
-	                                                <!--begin::Menu item-->
-	                                                <div class="menu-item px-3" data-kt-menu-trigger="hover"
-	                                                    data-kt-menu-placement="right-end">
-	                                                    <a href="#" class="menu-link px-3">
-	                                                        <span class="menu-title">Subscription</span>
-	                                                        <span class="menu-arrow"></span>
-	                                                    </a>
-	                                                    <!--begin::Menu sub-->
-	                                                    <div class="menu-sub menu-sub-dropdown w-175px py-4">
-	                                                        <!--begin::Menu item-->
-	                                                        <div class="menu-item px-3">
-	                                                            <a href="#" class="menu-link px-3">Plans</a>
-	                                                        </div>
-	                                                        <!--end::Menu item-->
-	                                                        <!--begin::Menu item-->
-	                                                        <div class="menu-item px-3">
-	                                                            <a href="#" class="menu-link px-3">Billing</a>
-	                                                        </div>
-	                                                        <!--end::Menu item-->
-	                                                        <!--begin::Menu item-->
-	                                                        <div class="menu-item px-3">
-	                                                            <a href="#" class="menu-link px-3">Statements</a>
-	                                                        </div>
-	                                                        <!--end::Menu item-->
-	                                                        <!--begin::Menu separator-->
-	                                                        <div class="separator my-2"></div>
-	                                                        <!--end::Menu separator-->
-	                                                        <!--begin::Menu item-->
-	                                                        <div class="menu-item px-3">
-	                                                            <div class="menu-content px-3">
-	                                                                <!--begin::Switch-->
-	                                                                <label
-	                                                                    class="form-check form-switch form-check-custom form-check-solid">
-	                                                                    <!--begin::Input-->
-	                                                                    <input class="form-check-input w-30px h-20px"
-	                                                                        type="checkbox" value="1" checked="checked"
-	                                                                        name="notifications" />
-	                                                                    <!--end::Input-->
-	                                                                    <!--end::Label-->
-	                                                                    <span
-	                                                                        class="form-check-label text-muted fs-6">Recuring</span>
-	                                                                    <!--end::Label-->
-	                                                                </label>
-	                                                                <!--end::Switch-->
-	                                                            </div>
-	                                                        </div>
-	                                                        <!--end::Menu item-->
-	                                                    </div>
-	                                                    <!--end::Menu sub-->
-	                                                </div>
-	                                                <!--end::Menu item-->
-	                                                <!--begin::Menu item-->
-	                                                <div class="menu-item px-3 my-1">
-	                                                    <a href="#" class="menu-link px-3">Settings</a>
-	                                                </div>
-	                                                <!--end::Menu item-->
-	                                            </div>
-	                                            <!--end::Menu 3-->
-	                                            <!--end::Menu-->
-	                                        </div>
 	                                    </div>
-	                                    <!--end::Header-->
-	                                    <!--begin::Body-->
 	                                    <div class="card-body py-3">
-	                                        <!--begin::Table container-->
 	                                        <div class="table-responsive">
-	                                            <!--begin::Table-->
 	                                            <table class="table align-middle gs-0 gy-3">
-	                                                <!--begin::Table head-->
 	                                                <thead>
 	                                                    <tr>
 	                                                        <th class="p-0 w-50px"></th>
@@ -198,14 +82,11 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <th class="p-0 min-w-40px"></th>
 	                                                    </tr>
 	                                                </thead>
-	                                                <!--end::Table head-->
-	                                                <!--begin::Table body-->
 	                                                <tbody>
 	                                                    <tr>
 	                                                        <td>
 	                                                            <div class="symbol symbol-50px me-2">
 	                                                                <span class="symbol-label bg-light-success">
-	                                                                    <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm002.svg-->
 	                                                                    <span
 	                                                                        class="svg-icon svg-icon-2x svg-icon-success">
 	                                                                        <svg xmlns="http://www.w3.org/2000/svg"
@@ -222,7 +103,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                                                fill="currentColor" />
 	                                                                        </svg>
 	                                                                    </span>
-	                                                                    <!--end::Svg Icon-->
 	                                                                </span>
 	                                                            </div>
 	                                                        </td>
@@ -239,7 +119,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <td>
 	                                                            <div class="symbol symbol-50px me-2">
 	                                                                <span class="symbol-label bg-light-danger">
-	                                                                    <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
 	                                                                    <span class="svg-icon svg-icon-2x svg-icon-danger">
 	                                                                        <svg xmlns="http://www.w3.org/2000/svg"
 	                                                                            width="24" height="24" viewBox="0 0 24 24"
@@ -257,7 +136,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                                                fill="currentColor" />
 	                                                                        </svg>
 	                                                                    </span>
-	                                                                    <!--end::Svg Icon-->
 	                                                                </span>
 	                                                            </div>
 	                                                        </td>
@@ -274,7 +152,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <td>
 	                                                            <div class="symbol symbol-50px me-2">
 	                                                                <span class="symbol-label bg-light-info">
-	                                                                    <!--begin::Svg Icon | path: icons/duotune/finance/fin006.svg-->
 	                                                                    <span class="svg-icon svg-icon-2x svg-icon-info">
 	                                                                        <svg xmlns="http://www.w3.org/2000/svg"
 	                                                                            width="24" height="24" viewBox="0 0 24 24"
@@ -287,7 +164,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                                                fill="currentColor" />
 	                                                                        </svg>
 	                                                                    </span>
-	                                                                    <!--end::Svg Icon-->
 	                                                                </span>
 	                                                            </div>
 	                                                        </td>
@@ -304,7 +180,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <td>
 	                                                            <div class="symbol symbol-50px me-2">
 	                                                                <span class="symbol-label bg-light-warning">
-	                                                                    <!--begin::Svg Icon | path: icons/duotune/abstract/abs027.svg-->
 	                                                                    <span
 	                                                                        class="svg-icon svg-icon-2x svg-icon-warning">
 	                                                                        <svg xmlns="http://www.w3.org/2000/svg"
@@ -318,7 +193,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                                                fill="currentColor" />
 	                                                                        </svg>
 	                                                                    </span>
-	                                                                    <!--end::Svg Icon-->
 	                                                                </span>
 	                                                            </div>
 	                                                        </td>
@@ -335,7 +209,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <td>
 	                                                            <div class="symbol symbol-50px me-2">
 	                                                                <span class="symbol-label bg-light-primary">
-	                                                                    <!--begin::Svg Icon | path: icons/duotune/abstract/abs042.svg-->
 	                                                                    <span
 	                                                                        class="svg-icon svg-icon-2x svg-icon-primary">
 	                                                                        <svg xmlns="http://www.w3.org/2000/svg"
@@ -349,7 +222,6 @@ $query14 = mysqli_query($con, $sql14);
 	                                                                                fill="currentColor" />
 	                                                                        </svg>
 	                                                                    </span>
-	                                                                    <!--end::Svg Icon-->
 	                                                                </span>
 	                                                            </div>
 	                                                        </td>
@@ -363,15 +235,10 @@ $query14 = mysqli_query($con, $sql14);
 	                                                        <td class="text-end text-dark fw-bolder fs-6 pe-0">500KB</td>
 	                                                    </tr>
 	                                                </tbody>
-	                                                <!--end::Table body-->
 	                                            </table>
-	                                            <!--end::Table-->
 	                                        </div>
-	                                        <!--end::Table container-->
 	                                    </div>
-	                                    <!--begin::Body-->
 	                                </div>
-	                                <!--end::Tables Widget 3-->
 	                            </div>
                     </div>
                     <div class="footer py-4 d-flex flex-column flex-md-row flex-stack" id="kt_footer">
