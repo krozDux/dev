eb3e9495b42e51384228cb62702639f1a7bd5a7b
Carlos <1971975680@undc.edu.pe>
2023-09-29 00:15:37 -0500

    as

--- contratos/index.php
+++ contratos/index.php
@@ -21,7 +21,7 @@
             "sScrollX": "100%",
             "sScrollXInner": "110%",
             "bScrollCollapse": true,
-            order: [[5, 'desc']],
+            order: [[4, 'desc']],
             buttons: [{
                     text: '<span class="svg-icon svg-icon-2 opacity-50">' +
                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">' +
