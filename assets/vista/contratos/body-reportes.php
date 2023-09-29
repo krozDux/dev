9f39e816190dc212de7387f253f16f4b149e928e
Carlos <1971975680@undc.edu.pe>
2023-09-29 02:05:30 -0500

    wa

--- assets/vista/contratos/body-reportes.php
+++ assets/vista/contratos/body-reportes.php
@@ -70,7 +70,7 @@ $query14 = mysqli_query($con, $sql14);
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
 	                                        <div class="table-responsive">
-                                                <div id="chart_div" style="height: 400px;"></div>
+                                                <div id="columnchart_material1" style="height: 400px;"></div>
 	                                        </div>
 	                                    </div>
 	                                </div>
@@ -85,7 +85,7 @@ $query14 = mysqli_query($con, $sql14);
 	                                    </div>
 	                                    <div class="card h-md-100 px-5">
                                             <div class="table-responsive">
-                                                <div id="columnchart_material" style="height: 400px;"></div>
+                                                <div id="columnchart_material2" style="height: 400px;"></div>
                                             </div>
 	                                    </div>
 	                                </div>
--- contratos/reportes.php
+++ contratos/reportes.php
@@ -11,34 +11,21 @@
     </script>
     <?php
     include('../config.php');
-    $sqlchart = ("SELECT
-    DATE_FORMAT(c.fechaFin, '%Y-%m') AS mes,
-    SUM(CASE WHEN u.rol = 'cliente' THEN 1 ELSE 0 END) AS total_registros_rol1,
-    SUM(CASE WHEN u.rol = 'proveedor' THEN 1 ELSE 0 END) AS total_registros_rol2,
-    CASE
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '01' THEN 'Enero'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '02' THEN 'Febrero'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '03' THEN 'Marzo'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '04' THEN 'Abril'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '05' THEN 'Mayo'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '06' THEN 'Junio'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '07' THEN 'Julio'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '08' THEN 'Agosto'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '09' THEN 'Septiembre'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '10' THEN 'Octubre'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '11' THEN 'Noviembre'
-        WHEN DATE_FORMAT(c.fechaFin, '%m') = '12' THEN 'Diciembre'
-    END AS nombre_mes
-FROM
-    contratos c
-INNER JOIN
-    usuarios u ON c.idUsuario = u.id
-WHERE
-    c.fechaFin >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH)
-GROUP BY
-    DATE_FORMAT(c.fechaFin, '%Y-%m')
-ORDER BY
-    mes;");
+    $sqlchart = ("SELECT DATE_FORMAT(c.fechaFin, '%Y-%m') AS mes, SUM(CASE WHEN u.rol = 'cliente' THEN 1 ELSE 0 END) AS total_registros_rol1, SUM(CASE WHEN u.rol = 'proveedor' THEN 1 ELSE 0 END) AS total_registros_rol2,
+        CASE
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '01' THEN 'Enero'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '02' THEN 'Febrero'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '03' THEN 'Marzo'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '04' THEN 'Abril'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '05' THEN 'Mayo'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '06' THEN 'Junio'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '07' THEN 'Julio'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '08' THEN 'Agosto'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '09' THEN 'Septiembre'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '10' THEN 'Octubre'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '11' THEN 'Noviembre'
+            WHEN DATE_FORMAT(c.fechaFin, '%m') = '12' THEN 'Diciembre'
+        END AS nombre_mes FROM contratos c INNER JOIN usuarios u ON c.idUsuario = u.id  WHERE c.fechaFin >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH) GROUP BY DATE_FORMAT(c.fechaFin, '%Y-%m') ORDER BY mes;");
     $querychart = mysqli_query($con, $sqlchart);
     ?>
     <script src="assets/plugins/global/plugins.bundle.js"></script>
@@ -66,53 +53,60 @@ ORDER BY
           
         };
 
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material2'));
 
         chart.draw(data, google.charts.Bar.convertOptions(options));
       }
     </script>
+    
+    </script>
+    <?php
+    include('../config.php');
+    $sqlchart1 = ("SELECT DATE_FORMAT(c.fechaInicio, '%Y-%m') AS mes, SUM(CASE WHEN u.rol = 'cliente' THEN 1 ELSE 0 END) AS total_registros_rol1, SUM(CASE WHEN u.rol = 'proveedor' THEN 1 ELSE 0 END) AS total_registros_rol2,
+        CASE
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '01' THEN 'Enero'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '02' THEN 'Febrero'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '03' THEN 'Marzo'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '04' THEN 'Abril'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '05' THEN 'Mayo'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '06' THEN 'Junio'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '07' THEN 'Julio'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '08' THEN 'Agosto'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '09' THEN 'Septiembre'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '10' THEN 'Octubre'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '11' THEN 'Noviembre'
+            WHEN DATE_FORMAT(c.fechaInicio, '%m') = '12' THEN 'Diciembre'
+        END AS nombre_mes FROM contratos c INNER JOIN usuarios u ON c.idUsuario = u.id  WHERE c.fechaInicio >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH) GROUP BY DATE_FORMAT(c.fechaInicio, '%Y-%m') ORDER BY mes;");
+    $querychart1 = mysqli_query($con, $sqlchart1);
+    ?>
+    <script src="assets/plugins/global/plugins.bundle.js"></script>
+    <script src="assets/js/scripts.bundle.js"></script>
+    <script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
+    <script src="assets/js/widgets.bundle.js"></script>
+    <script src="assets/js/custom/widgets.js"></script>
+    <script src="assets/js/custom/apps/chat/chat.js"></script>
+    <script src="assets/js/custom/utilities/modals/create-app.js"></script>
+    <script src="assets/js/custom/utilities/modals/users-search.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
+      google.charts.load('current', {'packages':['bar']});
       google.charts.setOnLoadCallback(drawChart);
+
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Dinosaur', 'Length'],
-          ['Acrocanthosaurus (top-spined lizard)', 12.2],
-          ['Albertosaurus (Alberta lizard)', 9.1],
-          ['Allosaurus (other lizard)', 12.2],
-          ['Apatosaurus (deceptive lizard)', 22.9],
-          ['Archaeopteryx (ancient wing)', 0.9],
-          ['Argentinosaurus (Argentina lizard)', 36.6],
-          ['Baryonyx (heavy claws)', 9.1],
-          ['Brachiosaurus (arm lizard)', 30.5],
-          ['Ceratosaurus (horned lizard)', 6.1],
-          ['Coelophysis (hollow form)', 2.7],
-          ['Compsognathus (elegant jaw)', 0.9],
-          ['Deinonychus (terrible claw)', 2.7],
-          ['Diplodocus (double beam)', 27.1],
-          ['Dromicelomimus (emu mimic)', 3.4],
-          ['Gallimimus (fowl mimic)', 5.5],
-          ['Mamenchisaurus (Mamenchi lizard)', 21.0],
-          ['Megalosaurus (big lizard)', 7.9],
-          ['Microvenator (small hunter)', 1.2],
-          ['Ornithomimus (bird mimic)', 4.6],
-          ['Oviraptor (egg robber)', 1.5],
-          ['Plateosaurus (flat lizard)', 7.9],
-          ['Sauronithoides (narrow-clawed lizard)', 2.0],
-          ['Seismosaurus (tremor lizard)', 45.7],
-          ['Spinosaurus (spiny lizard)', 12.2],
-          ['Supersaurus (super lizard)', 30.5],
-          ['Tyrannosaurus (tyrant lizard)', 15.2],
-          ['Ultrasaurus (ultra lizard)', 30.5],
-          ['Velociraptor (swift robber)', 1.8]]);
+          ['Mes', 'Cliente', 'Proveedor'],
+          <?php  while ($dataChart = mysqli_fetch_array($querychart)) { ?>
+          ['<?php echo $dataChart['nombre_mes']; ?>',<?php echo $dataChart['total_registros_rol1']; ?>,<?php echo $dataChart['total_registros_rol2']; ?>],
+          <?php } ?>
+        ]);
 
         var options = {
-          title: 'Lengths of dinosaurs, in meters',
-          legend: { position: 'none' },
+          
         };
 
-        var chart = new google.visualization.Histogram(document.getElementById('chart_div'));
-        chart.draw(data, options);
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material1'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
       }
     </script>
 </body>
