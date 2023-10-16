a67b04455752ab81339b110b3370229d98964ead
Carlos <1971975680@undc.edu.pe>
2023-10-16 01:09:59 -0500

    wa

--- proyectos/index.php
+++ proyectos/index.php
@@ -31,10 +31,7 @@
             maxItems: 20,           // <- mixumum allowed rendered suggestions
             classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
             enabled: 0,             // <- show suggestions on focus
-            closeOnSelect: true,    // <- do not hide the suggestions dropdown once an item has been selected
-            template: function (item) {
-            return item.label;
-        },
+            closeOnSelect: true    // <- do not hide the suggestions dropdown once an item has been selected
         }
         });
         $('#kt_table_users').DataTable({
