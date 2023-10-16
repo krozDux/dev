520c2fdf83e383690bdd876395924f8b7c9e6754
Carlos <1971975680@undc.edu.pe>
2023-10-16 01:08:21 -0500

    wa

--- proyectos/index.php
+++ proyectos/index.php
@@ -25,13 +25,16 @@
             value: "<?php echo $dataproy1['id']; ?>", // Utiliza el campo 'id'
             label: "<?php echo $dataproy1['nombres'] . ' ' . $dataproy1['apellidos']; ?>"
         },
-    <?php } ?>],        
-    maxTags: 10,
+        <?php } ?>],        
+        maxTags: 10,
         dropdown: {
             maxItems: 20,           // <- mixumum allowed rendered suggestions
             classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
             enabled: 0,             // <- show suggestions on focus
-            closeOnSelect: true    // <- do not hide the suggestions dropdown once an item has been selected
+            closeOnSelect: true,    // <- do not hide the suggestions dropdown once an item has been selected
+            template: function (item) {
+            return item.label;
+        },
         }
         });
         $('#kt_table_users').DataTable({
