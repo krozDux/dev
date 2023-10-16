72eece87c40df9617d4e056c1261fa0ac823fa11
Carlos <1971975680@undc.edu.pe>
2023-10-16 01:12:11 -0500

    wa

--- proyectos/index.php
+++ proyectos/index.php
@@ -21,11 +21,12 @@
     $(document).ready(function() {
         var input = document.querySelector('input[name="tags1"]'),
         tagify = new Tagify(input, {
-            whitelist: [<?php while ($dataproy1 = mysqli_fetch_array($queryproy1)) { ?>{
-            value: "<?php echo $dataproy1['nombres'] . ' ' . $dataproy1['apellidos']; ?>", // Utiliza el campo 'id'
+            whitelist: [{
+            <?php while ($dataproy1 = mysqli_fetch_array($queryproy1)) { ?>
+            value: "<?php echo $dataproy1['nombres'] . ' ' . $dataproy1['apellidos']; ?>",
             label: "<?php echo $dataproy1['id']; ?>"
-        },
-        <?php } ?>],        
+            <?php } ?>},
+        ],        
         maxTags: 10,
         dropdown: {
             maxItems: 20,           // <- mixumum allowed rendered suggestions
