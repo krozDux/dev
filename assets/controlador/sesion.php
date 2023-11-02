720dc9a85318960b971a080755cd2fd69493f381
Carlos <1971975680@undc.edu.pe>
2023-11-02 02:18:01 -0500

    wa

--- assets/controlador/sesion.php
+++ assets/controlador/sesion.php
@@ -1,23 +1,23 @@
 <?php
 include('../config.php');
-$inactivityTimeout = 5000 * 60; // 5 minutos
-ini_set('session.gc_maxlifetime', $inactivityTimeout);
-session_set_cookie_params($inactivityTimeout);
-session_start();
-// Verificar si la sesión ha expirado por inactividad
-if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $inactivityTimeout) {
-    // La sesión ha expirado por inactividad, destruye la sesión y redirige al usuario
-    session_unset();
-    session_destroy();
-    header('Location: ../login/indexga.php');
-    exit();
-}
+// $inactivityTimeout = 5000 * 60; // 5 minutos
+// ini_set('session.gc_maxlifetime', $inactivityTimeout);
+// session_set_cookie_params($inactivityTimeout);
+// session_start();
+// // Verificar si la sesión ha expirado por inactividad
+// if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $inactivityTimeout) {
+//     // La sesión ha expirado por inactividad, destruye la sesión y redirige al usuario
+//     session_unset();
+//     session_destroy();
+//     header('Location: ../login/index.php');
+//     exit();
+// }
 
-// Actualiza el tiempo de actividad
-$_SESSION['last_activity'] = time();
+// // Actualiza el tiempo de actividad
+// $_SESSION['last_activity'] = time();
 
 if (!isset($_SESSION['email'])) {
-    header('Location: ../login/ga.php');
+    header('Location: ../login/index.php');
     exit();
 } else {
 	$session_email = $_SESSION['email'];
