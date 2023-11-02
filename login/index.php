59e7946670973d955f8f117844657fdd075f1e5d
Carlos <1971975680@undc.edu.pe>
2023-11-02 02:09:19 -0500

    wa

--- login/index.php
+++ login/index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
+<?php include_once '../assets/controlador/sesion.php'?>
+
 <?php include_once '../assets/vista/login/head-login.php'?>
 
 <body id="kt_body" class="bg-body">
