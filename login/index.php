9a5ab24c6a3bbdbcb5586b331312b44b788ca5ab
Carlos <1971975680@undc.edu.pe>
2023-11-02 02:03:08 -0500

    wa

--- login/index.php
+++ login/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php include_once '../assets/controlador/sesion.php'?>
 <?php include_once '../assets/vista/login/head-login.php'?>
+<?php include_once '../assets/controlador/sesion.php'?>
 <body id="kt_body" class="bg-body">
     <?php include_once '../assets/vista/login/body-login.php'?>
     <script src="assets/plugins/global/plugins.bundle.js"></script>
