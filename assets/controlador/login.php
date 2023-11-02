9c57f5ed4ab5078713368fb7813fd4cd24fa204c
Carlos <1971975680@undc.edu.pe>
2023-11-02 01:49:22 -0500

    wa

--- assets/controlador/login.php
+++ assets/controlador/login.php
@@ -35,13 +35,22 @@ if (!empty($_POST['btningresar'])) {
                     </div>
                 </div> ';
             } else {
-                
                 $_SESSION['email'] = $email;
                 if (!isset($_SESSION['email'])) {
                     header('Location: ../login/index.php');
                     exit();
                 } else {
                     session_start();
+                    $session_email = $_SESSION['email'];
+                    $sqlUser= ("SELECT * FROM `usuarios` where email = '$session_email'");
+                    $queryUser = mysqli_query($con, $sqlUser);
+                    while ($dataUser = mysqli_fetch_array($queryUser)) {
+                        $session_id = $dataUser['id']; 
+                        $session_nombres = $dataUser['nombres']; 
+                        $session_apellidos = $dataUser['apellidos'];
+                        $session_imagen = $dataUser['imagen'];
+                        $session_rol = $dataUser['rol'];
+                    }
                     header("location:../panel/index.php");
                 }
             }
