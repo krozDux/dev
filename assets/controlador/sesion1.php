8fcb3be2abb4e6ae714f5c0f77cb959327ccdca5
Carlos <1971975680@undc.edu.pe>
2023-11-02 02:16:27 -0500

    wa

--- assets/controlador/sesion1.php
+++ assets/controlador/sesion1.php
@@ -1,23 +1,8 @@
 <?php
 if (!empty($_POST['btningresar'])) {
     $_SESSION['email'] = $email;
-	$session_email = $_SESSION['email'];
-    $sqlUser= ("SELECT * FROM `usuarios` where email = '$session_email'");
-    $queryUser = mysqli_query($con, $sqlUser);
-    while ($dataUser = mysqli_fetch_array($queryUser)) {
-        $session_id = $dataUser['id']; 
-        $session_nombres = $dataUser['nombres']; 
-        $session_apellidos = $dataUser['apellidos'];
-        $session_imagen = $dataUser['imagen'];
-        $session_rol = $dataUser['rol'];
-    }
     header('Location: ../panel/index.php');
-}
-
-if (!empty($_POST['unlogin'])) {
-    session_unset();
-    session_destroy();
-    header('Location: ../login/index.php');
     exit();
 }
+
 ?>
