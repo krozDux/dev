76cd06333b97e4ad68c4e7a65a2da5be743ee623
Carlos <1971975680@undc.edu.pe>
2023-11-02 01:59:49 -0500

    wa

--- assets/controlador/sesion.php
+++ assets/controlador/sesion.php
@@ -1,43 +1,41 @@
 <?php
-session_start();
+include('../config.php');
+// $inactivityTimeout = 5000 * 60; // 5 minutos
+// ini_set('session.gc_maxlifetime', $inactivityTimeout);
+// session_set_cookie_params($inactivityTimeout);
+// session_start();
+// // Verificar si la sesión ha expirado por inactividad
+// if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $inactivityTimeout) {
+//     // La sesión ha expirado por inactividad, destruye la sesión y redirige al usuario
+//     session_unset();
+//     session_destroy();
+//     header('Location: ../login/index.php');
+//     exit();
+// }
 
-// Verificar si el usuario no ha iniciado sesión
+// // Actualiza el tiempo de actividad
+// $_SESSION['last_activity'] = time();
+
 if (!isset($_SESSION['email'])) {
-    // Verificar si estamos en la página de inicio de sesión para evitar un bucle de redirección
-    $currentPage = basename($_SERVER['PHP_SELF']);
-    if ($currentPage !== 'index.php') {
-        header('Location: ../login/index.php');
-        exit();
+    header('Location: ../login/index.php');
+    exit();
+} else {
+	$session_email = $_SESSION['email'];
+    $sqlUser= ("SELECT * FROM `usuarios` where email = '$session_email'");
+    $queryUser = mysqli_query($con, $sqlUser);
+    while ($dataUser = mysqli_fetch_array($queryUser)) {
+        $session_id = $dataUser['id']; 
+        $session_nombres = $dataUser['nombres']; 
+        $session_apellidos = $dataUser['apellidos'];
+        $session_imagen = $dataUser['imagen'];
+        $session_rol = $dataUser['rol'];
     }
 }
-// Verifica si el usuario no ha iniciado sesión
-if (!isset($_SESSION['email'])) {
-    header('Location: ../login/index.php');
-    exit();
-}
 
-// Recupera los datos del usuario
-include('../config.php');
-$session_email = $_SESSION['email'];
-$sqlUser = "SELECT * FROM `usuarios` WHERE email = '$session_email'";
-$queryUser = mysqli_query($con, $sqlUser);
-
-if ($queryUser && $dataUser = mysqli_fetch_array($queryUser)) {
-    $session_id = $dataUser['id']; 
-    $session_nombres = $dataUser['nombres']; 
-    $session_apellidos = $dataUser['apellidos'];
-    $session_imagen = $dataUser['imagen'];
-    $session_rol = $dataUser['rol'];
-} else {
-    // Manejo de error si no se pueden recuperar los datos del usuario
-    header('Location: ../login/index.php');
-    exit();
-}
-
-// Cerrar sesión
 if (!empty($_POST['unlogin'])) {
     session_unset();
     session_destroy();
     header('Location: ../login/index.php');
     exit();
 }
+?>
