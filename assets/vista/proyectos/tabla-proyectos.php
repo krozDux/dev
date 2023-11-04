13597702f4119ab6d99bf547d3e39d433a27896b
Carlos <1971975680@undc.edu.pe>
2023-11-03 21:17:33 -0500

    waw

--- assets/vista/proyectos/tabla-proyectos.php
+++ assets/vista/proyectos/tabla-proyectos.php
@@ -3,60 +3,74 @@ include('../config.php');
 $sql1 = ("SELECT * FROM usuarios WHERE estado = 1 and rol='cliente'");
 $query1 = mysqli_query($con, $sql1);
 ?>
-<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
-											<thead>
-												<tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
-													<th class="min-w-120px"> Imagen</th>
-													<th class="min-w-125px">Nombres</th>
-													<th data-priority="1" class="min-w-125px">Apellidos</th>
-													<th class="min-w-125px">Email</th>
-													<th class="min-w-125px">Dirección</th>
-													<th class="min-w-125px">Número</th>
-													<th data-priority="2" class="min-w-125px">Opciones</th>
-												</tr>
-											</thead>
-											<tbody class="text-gray-600 fw-bold">
-											<?php 
-												$i = 1;
-												while ($dataUsuario1 = mysqli_fetch_array($query1)) { ?>
-												<tr>
-													<td class="d-flex align-items-center">
-														<div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
-															<a>
-																<div class="symbol-label">
-																	<img src="assets/media/avatars/<?php echo $dataUsuario1['imagen']; ?>" alt="<?php echo $dataUsuario1['nombres']; ?> <?php echo $dataUsuario1['apellidos']; ?>" class="w-100" />
-																</div>
-															</a>
-														</div>
-													</td>
-													<td><?php echo $dataUsuario1['nombres']; ?></td>
-													<td><?php echo $dataUsuario1['apellidos']; ?></td>
-													<td><?php echo $dataUsuario1['email']; ?></td>
-													<td><?php echo $dataUsuario1['direccion']; ?></td>
-													<?php 
-													if ($dataUsuario1['numero'] == "-") { ?>
-													<td><?php echo $dataUsuario1['numero']; ?></td>
-													<?php } else { ?>
-														<td><a href="https://wa.me/51<?php echo $dataUsuario1['numero']; ?>"><?php echo $dataUsuario1['numero']; ?></a></td>
-													<?php } ?>
-													
-													
-													<td>
-																<buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm edit-usuario" 
-																data-id="<?php echo $dataUsuario1['id']; ?>"
-																data-email="<?php echo $dataUsuario1['email']; ?>"
-																data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
-																data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
-																data-direccion="<?php echo $dataUsuario1['direccion']; ?>"
-																data-numero="<?php echo $dataUsuario1['numero']; ?>"
-																data-imagen="<?php echo $dataUsuario1['imagen']; ?>"><span class="bi bi-pencil-fill fs-7 opacity-75"></buttton>
-																<buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm del-usuario" 
-																data-id="<?php echo $dataUsuario1['id']; ?>"
-																data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
-																data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
-																data-rol="<?php echo $dataUsuario1['rol']; ?>"><span class="bi bi-trash-fill fs-7 opacity-75"></span></buttton>
-													</td>
-												</tr>
-												<?php } ?>
-											</tbody>
-										</table>
+<div class="row g-6 g-xl-9 pt-3">
+    <div class="col-md-6 col-xl-4" style="border: 2px solid #e9edf1; border-radius: 12px;">
+        <a href="/metronic8/demo14/../demo14/apps/projects/project.html" class="card border-hover-primary">
+            <div class="card-header border-0 pt-9 pb-0">
+                <div class="card-title m-0">
+                    <div class="fs-3 fw-bold text-gray-900">
+					Fitnes App
+                    </div>
+                </div>
+
+                <div class="card-toolbar">
+                    <span class="badge badge-light-primary fw-bold me-auto px-4 py-3">In Progress</span>
+                </div>
+            </div>
+
+            <div class="card-body pt-1">
+                <p class="text-gray-500 fw-semibold fs-5 mt-1 mb-7">
+                    CRM App application to HR efficiency </p>
+                <div class="d-flex flex-wrap mb-5">
+                    <!--begin::Due-->
+                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-7 mb-3">
+                        <div class="fs-6 text-gray-800 fw-bold">Mar 10, 2023</div>
+                        <div class="fw-semibold text-gray-500">Fecha de Inicio</div>
+                    </div>
+                    <!--end::Due-->
+
+                    <!--begin::Budget-->
+                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 mb-3">
+                        <div class="fs-6 text-gray-800 fw-bold">Mar 10, 2023</div>
+                        <div class="fw-semibold text-gray-500">Fecha limite</div>
+                    </div>
+                    <!--end::Budget-->
+                </div>
+                <!--end::Info-->
+
+                <!--begin::Progress-->
+                <div class="h-4px w-100 bg-light mb-5" data-bs-toggle="tooltip" aria-label="This project 50% completed"
+                    data-bs-original-title="This project 50% completed" data-kt-initialized="1">
+                    <div class="bg-primary rounded h-4px" role="progressbar" style="width: 50%" aria-valuenow=" 50"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <!--end::Progress-->
+
+                <!--begin::Users-->
+                <div class="symbol-group symbol-hover">
+                    <!--begin::User-->
+                    <div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" aria-label="Emma Smith"
+                        data-bs-original-title="Emma Smith" data-kt-initialized="1">
+                        <img alt="Pic" src="/metronic8/demo14/assets/media/avatars/300-6.jpg">
+                    </div>
+                    <!--begin::User-->
+                    <!--begin::User-->
+                    <div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip" aria-label="Rudy Stone"
+                        data-bs-original-title="Rudy Stone" data-kt-initialized="1">
+                        <img alt="Pic" src="/metronic8/demo14/assets/media/avatars/300-1.jpg">
+                    </div>
+                    <!--begin::User-->
+                    <!--begin::User-->
+                    <div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip"
+                        data-bs-original-title="Susan Redwood" data-kt-initialized="1">
+                        <span class="symbol-label bg-primary text-inverse-primary fw-bold">C</span>
+                    </div>
+                    <!--begin::User-->
+                </div>
+                <!--end::Users-->
+            </div>
+            <!--end:: Card body-->
+        </a>
+        <!--end::Card-->
+    </div>
+</div>
