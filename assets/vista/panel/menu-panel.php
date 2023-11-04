64d48031e06cf02120b3dbfd80991f683c5b9fc0
Carlos <1971975680@undc.edu.pe>
2023-11-03 20:38:39 -0500

    wa

--- assets/vista/panel/menu-panel.php
+++ assets/vista/panel/menu-panel.php
@@ -35,7 +35,7 @@
                         </div>
                     </div>
                 </div>
-                <?php  if ($session_rol = "invitado") { ?>
+                <?php  if ($session_rol == "invitado") { ?>
                 <div class="menu-item">
                     <div class="menu-content pt-8 pb-2">
                         <span class="menu-section text-muted text-uppercase fs-8 ls-1 mt-0">GESTIÓN DE CONTRATOS</span>
