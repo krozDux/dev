65be9dd21d68f7fd439d82e9c184901e348c4628
Carlos <1971975680@undc.edu.pe>
2023-11-05 18:19:31 -0500

    wa

--- proyectos/index.php
+++ proyectos/index.php
@@ -36,7 +36,7 @@
 							'<path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Z"/>'+
 							'</svg>'+
 							'</span>Vista',
-						className: 'btn btn-primary',
+						className: 'btn btn-primary vista-proyecto1',
 					},
                 {
                     extend: 'excelHtml5',
