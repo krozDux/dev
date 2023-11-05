6459571ee601afd320209a57c308b413e40c38e6
Carlos <1971975680@undc.edu.pe>
2023-11-05 16:42:58 -0500

    wa

--- assets/vista/proyectos/tabla-proyectos.php
+++ assets/vista/proyectos/tabla-proyectos.php
@@ -6,20 +6,17 @@
 include('../config.php');
 $sql1 = ("SELECT * FROM proyectos JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto WHERE proyectosInfo.idUsuario = '$session_id'");
 $query1 = mysqli_query($con, $sql1);
-include('../config.php');
 $sql2 = ("SELECT * FROM proyectos JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto WHERE proyectosInfo.idUsuario = '$session_id'");
 $query2 = mysqli_query($con, $sql2);
 ?>
 <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
 											<thead>
 												<tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
-													<th class="min-w-120px"> Imagen</th>
-													<th class="min-w-125px">Nombres</th>
-													<th data-priority="1" class="min-w-125px">Apellidos</th>
-													<th class="min-w-125px">Email</th>
-													<th class="min-w-125px">Dirección</th>
-													<th class="min-w-125px">Número</th>
-													<th data-priority="2" class="min-w-125px">Opciones</th>
+													<th class="min-w-125px">Nombre</th>
+													<th class="min-w-125px">Fecha inicio</th>
+													<th class="min-w-125px">Fecha Límite</th>
+													<th class="min-w-125px">Descripción</th>
+													<th class="min-w-125px">Estado</th>
 												</tr>
 											</thead>
 											<tbody class="text-gray-600 fw-bold">
@@ -27,42 +24,22 @@ $query2 = mysqli_query($con, $sql2);
 												$i = 1;
 												while ($dataUsuario1 = mysqli_fetch_array($query1)) { ?>
 												<tr>
-													<td class="d-flex align-items-center">
-														<div class="symbol symbol-circle symbol-50px overflow-hidden me-3">
-															<a>
-																<div class="symbol-label">
-																	<img src="assets/media/avatars/<?php echo $dataUsuario1['imagen']; ?>" alt="<?php echo $dataUsuario1['nombres']; ?> <?php echo $dataUsuario1['apellidos']; ?>" class="w-100" />
-																</div>
-															</a>
-														</div>
-													</td>
-													<td><?php echo $dataUsuario1['nombres']; ?></td>
-													<td><?php echo $dataUsuario1['apellidos']; ?></td>
-													<td><?php echo $dataUsuario1['email']; ?></td>
-													<td><?php echo $dataUsuario1['direccion']; ?></td>
-													<?php 
-													if ($dataUsuario1['numero'] == "-") { ?>
-													<td><?php echo $dataUsuario1['numero']; ?></td>
+													<td><?php echo $dataUsuario1['nombre']; ?></td>
+													<td><?php echo $dataUsuario1['fechaInicio']; ?></td>
+													<td><?php echo $dataUsuario1['fechaFin']; ?></td>
+                                                    <?php 
+													if ($dataUsuario1['descripcion'] == "") { ?>
+													<td>-</td>
 													<?php } else { ?>
-														<td><a href="https://wa.me/51<?php echo $dataUsuario1['numero']; ?>"><?php echo $dataUsuario1['numero']; ?></a></td>
+													<td><?php echo $dataUsuario1['descripcion']; ?></td>
 													<?php } ?>
 													
-													
-													<td>
-																<buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm edit-usuario" 
-																data-id="<?php echo $dataUsuario1['id']; ?>"
-																data-email="<?php echo $dataUsuario1['email']; ?>"
-																data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
-																data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
-																data-direccion="<?php echo $dataUsuario1['direccion']; ?>"
-																data-numero="<?php echo $dataUsuario1['numero']; ?>"
-																data-imagen="<?php echo $dataUsuario1['imagen']; ?>"><span class="bi bi-pencil-fill fs-7 opacity-75"></buttton>
-																<buttton type="button" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm del-usuario" 
-																data-id="<?php echo $dataUsuario1['id']; ?>"
-																data-nombres="<?php echo $dataUsuario1['nombres']; ?>"
-																data-apellidos="<?php echo $dataUsuario1['apellidos']; ?>"
-																data-rol="<?php echo $dataUsuario1['rol']; ?>"><span class="bi bi-trash-fill fs-7 opacity-75"></span></buttton>
-													</td>
+													<?php 
+													if ($dataUsuario1['estado'] == "1") { ?>
+													<td>Activo</td>
+													<?php } else { ?>
+													<td>Inactivo</td>
+													<?php } ?>
 												</tr>
 												<?php } ?>
 											</tbody>
@@ -93,7 +70,7 @@ $query2 = mysqli_query($con, $sql2);
                                     ?>
                                     <div class="card-body pt-1">
                                         <p class="text-gray-500 fw-semibold fs-5 mt-1 mb-7">
-                                            CRM App application to HR efficiency </p>
+                                        <?php echo $dataUsuario2['descripcion']; ?></p>
                                         <div class="d-flex flex-wrap mb-5">
                                             <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-7 mb-3">
                                                 <div class="fs-6 text-gray-800 fw-bold"><?php echo $fechaInicioF?></div>
