d26d8b60b48278e04745c8752f458f1323afd9b9
Carlos <1971975680@undc.edu.pe>
2023-11-05 18:00:13 -0500

    wa

--- assets/vista/proyectos/tabla-proyectos1.php
+++ assets/vista/proyectos/tabla-proyectos1.php
@@ -10,10 +10,10 @@ include('../config.php');
 $sql2 = ("SELECT * FROM proyectos JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto");
 $query2 = mysqli_query($con, $sql2);
 ?>
-<div class="content flex-column-fluid" id="kt_content" hidden>
+<div class="content flex-column-fluid" id="kt_content">
                         <div class="card">
                             <div class="card-body py-4">
-<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users" >
+<table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users" hidden>
 											<thead>
 												<tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
 													<th class="min-w-125px">Nombre</th>
