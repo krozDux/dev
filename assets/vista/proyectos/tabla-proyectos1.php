e48b80d1b866e52eeb16d1899df4082015550e1e
Carlos <1971975680@undc.edu.pe>
2023-11-06 23:40:35 -0500

    wa

--- assets/vista/proyectos/tabla-proyectos1.php
+++ assets/vista/proyectos/tabla-proyectos1.php
@@ -57,7 +57,7 @@ $query2 = mysqli_query($con, $sql2);
                         
     while ($dataUsuario2 = mysqli_fetch_array($query2)) {
         echo $dataUsuario2['id'];
-        var_dump($query2);
+        
         $idProyecto = $dataUsuario2['id'];
         include('../config.php');
         $sql3 = ("SELECT * FROM proyectos JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto LEFT JOIN usuarios ON proyectosInfo.idUsuario = usuarios.id WHERE proyectosInfo.idProyecto = '$idProyecto' AND proyectosInfo.estado='1';");
