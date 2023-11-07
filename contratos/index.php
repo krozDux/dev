1f3768329c50482c44c4814aeca10c9778ee7e53
Carlos <1971975680@undc.edu.pe>
2023-11-06 22:17:04 -0500

    wa

--- contratos/index.php
+++ contratos/index.php
@@ -42,7 +42,7 @@
                         '</span>Exportar</button>',
                     className: 'btn btn-primary ',
                     exportOptions: {
-                        columns: [1, 2, 3, 4, 5, 6, 7]
+                        columns: [1, 2, 3, 4, 6, 7, 5]
                     },
                     autoFilter: true,
                     sheetName: 'Reporte - Usuarios'
