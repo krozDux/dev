071bb271005c74df448bf65b9cd856f7df949226
Carlos <1971975680@undc.edu.pe>
2023-11-06 22:41:21 -0500

    wa

--- assets/vista/proyectos/body-proyectos.php
+++ assets/vista/proyectos/body-proyectos.php
@@ -101,7 +101,11 @@
                     </div>
                     <?php
                     include('../config.php');
-                    $sql1 = ("SELECT * FROM usuarios INNER JOIN contratos ON usuarios.id = contratos.idUsuario where usuarios.rol='proveedor'");
+                    $sql1 = ("SELECT * 
+                    FROM usuarios 
+                    INNER JOIN contratos ON usuarios.id = contratos.idUsuario 
+                    WHERE usuarios.rol = 'proveedor' 
+                      AND contratos.fechaFin < CURDATE();");
                     $query1 = mysqli_query($con, $sql1);
                     $sql2 = ("SELECT * FROM usuarios INNER JOIN contratos ON usuarios.id = contratos.idUsuario where usuarios.rol='cliente'");
                     $query2 = mysqli_query($con, $sql2);
