52992dafbc8490fa28bb41829d0745965e1286d1
Carlos <1971975680@undc.edu.pe>
2023-11-09 20:32:22 -0500

    wa

---
+++ assets/vista/proyectos/body-proyectos2.php
@@ -0,0 +1,208 @@
+<?php  if ($session_rol != "invitado") {?>
+<?php include '../assets/controlador/proyectos.php'?>
+<div class="d-flex flex-column flex-root">
+    <div class="page d-flex flex-row flex-column-fluid">
+        <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+            <div id="kt_header" class="header">
+                <div class="container-fluid d-flex flex-stack">
+                    <div class="d-flex align-items-center me-5">
+                        <div class="d-lg-none btn btn-icon btn-active-color-white w-30px h-30px ms-n2 me-3"
+                            id="kt_aside_toggle">
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <path
+                                        d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z"
+                                        fill="currentColor" />
+                                    <path opacity="0.3"
+                                        d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z"
+                                        fill="currentColor" />
+                                </svg>
+                            </span>
+                        </div>
+                        <a href="panel/index.php">
+                            <img alt="Logo" src="assets/media/logos/logo-2.svg" class="h-25px h-lg-30px" />
+                        </a>
+                    </div>
+                    <div class="d-flex align-items-center flex-shrink-0">
+                        <div class="d-flex align-items-center ms-1">
+                            <div class="btn btn-icon btn-color-white bg-hover-white bg-hover-opacity-10 w-30px h-30px h-40px w-40px"
+                                id="kt_activities_toggle">
+                                <span class="svg-icon svg-icon-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none">
+                                        <rect x="8" y="9" width="3" height="10" rx="1.5" fill="currentColor" />
+                                        <rect opacity="0.5" x="13" y="5" width="3" height="14" rx="1.5"
+                                            fill="currentColor" />
+                                        <rect x="18" y="11" width="3" height="8" rx="1.5" fill="currentColor" />
+                                        <rect x="3" y="13" width="3" height="6" rx="1.5" fill="currentColor" />
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <?php include_once '../assets/controlador/header.php'?>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column-fluid">
+
+                <?php include_once 'menu-proyectos.php'?>
+
+                <div class="d-flex flex-column flex-column-fluid container-fluid">
+                    <div class="toolbar mb-2 mb-lg-2" id="kt_toolbar">
+                        <div class="page-title d-flex flex-column me-3 mb-1">
+                            <h1 class="d-flex text-dark fw-bolder my-1 fs-3">Proyectos</h1>
+                        </div>
+                    </div>
+                    <!-- <div class="content flex-column-fluid" id="kt_content">
+                        <div class="card">
+                            <div class="card-body py-4"> -->
+                    <?php include_once 'tabla-proyectos.php'?>
+                    <!-- </div>
+                        </div>
+                    </div> -->
+                    <div class="footer py-4 d-flex flex-column flex-md-row flex-stack" id="kt_footer">
+                        <?php include_once 'footer-proyectos.php'?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="kt_modal_new_target" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <div class="modal-content rounded">
+            <div class="modal-header pb-0 border-0 justify-content-end">
+                <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                    <span class="svg-icon svg-icon-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
+                <form class="form" method="POST" enctype="multipart/form-data">
+                    <div class="mb-13 text-center">
+                        <h1 class="mb-3">CREAR NUEVO PROYECTO</h1>
+                    </div>
+                    <div class="d-flex flex-column mb-8 fv-row">
+                        <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                            <span class="required">Nombre del proyecto</span>
+                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                title="Specify a target name for future usage and reference"></i>
+                        </label>
+                        <input type="text" class="form-control form-control-solid" placeholder="Nombre" name="nombre"
+                            required />
+                    </div>
+                    <?php
+                    include('../config.php');
+                    $sql1 = ("SELECT * 
+                    FROM usuarios 
+                    INNER JOIN contratos ON usuarios.id = contratos.idUsuario 
+                    WHERE usuarios.rol = 'proveedor' 
+                      AND contratos.fechaFin > CURDATE();");
+                    $query1 = mysqli_query($con, $sql1);
+                    $sql2 = ("SELECT * FROM usuarios INNER JOIN contratos ON usuarios.id = contratos.idUsuario where usuarios.rol='cliente'");
+                    $query2 = mysqli_query($con, $sql2);
+                    ?>
+                    <div class="row g-9 mb-8">
+                        <div class="col-md-6 fv-row">
+                            <label class="required fs-6 fw-bold mb-2">Encargado del proyecto</label>
+                            <select class="form-select form-select-solid" data-placeholder="Seleccionar encargado"
+                                name="encargado" required>
+                                <option value="">Seleccionar encargado</option>
+                                <?php 
+                                while ($dataUsuario1 = mysqli_fetch_array($query1)) { ?>
+                                <?php  if ($dataUsuario1['fechaFin'] != "" and $dataUsuario1['fechaInicio'] != "") { ?>
+                                <option value="<?php echo $dataUsuario1['id']; ?>">
+                                    <?php echo $dataUsuario1['nombres']; ?> <?php echo $dataUsuario1['apellidos']; ?>
+                                </option>
+                                <?php } } ?>
+                            </select>
+
+                            <label class="required fs-6 fw-bold mb-2">Fecha de inicio</label>
+                            <div class="position-relative d-flex align-items-center">
+                                <span class="svg-icon svg-icon-2 position-absolute mx-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none">
+                                        <path opacity="0.3"
+                                            d="M21 22H3C2.4 22 2 21.6 2 21V5C2 4.4 2.4 4 3 4H21C21.6 4 22 4.4 22 5V21C22 21.6 21.6 22 21 22Z"
+                                            fill="currentColor" />
+                                        <path
+                                            d="M6 6C5.4 6 5 5.6 5 5V3C5 2.4 5.4 2 6 2C6.6 2 7 2.4 7 3V5C7 5.6 6.6 6 6 6ZM11 5V3C11 2.4 10.6 2 10 2C9.4 2 9 2.4 9 3V5C9 5.6 9.4 6 10 6C10.6 6 11 5.6 11 5ZM15 5V3C15 2.4 14.6 2 14 2C13.4 2 13 2.4 13 3V5C13 5.6 13.4 6 14 6C14.6 6 15 5.6 15 5ZM19 5V3C19 2.4 18.6 2 18 2C17.4 2 17 2.4 17 3V5C17 5.6 17.4 6 18 6C18.6 6 19 5.6 19 5Z"
+                                            fill="currentColor" />
+                                        <path
+                                            d="M8.8 13.1C9.2 13.1 9.5 13 9.7 12.8C9.9 12.6 10.1 12.3 10.1 11.9C10.1 11.6 10 11.3 9.8 11.1C9.6 10.9 9.3 10.8 9 10.8C8.8 10.8 8.59999 10.8 8.39999 10.9C8.19999 11 8.1 11.1 8 11.2C7.9 11.3 7.8 11.4 7.7 11.6C7.6 11.8 7.5 11.9 7.5 12.1C7.5 12.2 7.4 12.2 7.3 12.3C7.2 12.4 7.09999 12.4 6.89999 12.4C6.69999 12.4 6.6 12.3 6.5 12.2C6.4 12.1 6.3 11.9 6.3 11.7C6.3 11.5 6.4 11.3 6.5 11.1C6.6 10.9 6.8 10.7 7 10.5C7.2 10.3 7.49999 10.1 7.89999 10C8.29999 9.90003 8.60001 9.80003 9.10001 9.80003C9.50001 9.80003 9.80001 9.90003 10.1 10C10.4 10.1 10.7 10.3 10.9 10.4C11.1 10.5 11.3 10.8 11.4 11.1C11.5 11.4 11.6 11.6 11.6 11.9C11.6 12.3 11.5 12.6 11.3 12.9C11.1 13.2 10.9 13.5 10.6 13.7C10.9 13.9 11.2 14.1 11.4 14.3C11.6 14.5 11.8 14.7 11.9 15C12 15.3 12.1 15.5 12.1 15.8C12.1 16.2 12 16.5 11.9 16.8C11.8 17.1 11.5 17.4 11.3 17.7C11.1 18 10.7 18.2 10.3 18.3C9.9 18.4 9.5 18.5 9 18.5C8.5 18.5 8.1 18.4 7.7 18.2C7.3 18 7 17.8 6.8 17.6C6.6 17.4 6.4 17.1 6.3 16.8C6.2 16.5 6.10001 16.3 6.10001 16.1C6.10001 15.9 6.2 15.7 6.3 15.6C6.4 15.5 6.6 15.4 6.8 15.4C6.9 15.4 7.00001 15.4 7.10001 15.5C7.20001 15.6 7.3 15.6 7.3 15.7C7.5 16.2 7.7 16.6 8 16.9C8.3 17.2 8.6 17.3 9 17.3C9.2 17.3 9.5 17.2 9.7 17.1C9.9 17 10.1 16.8 10.3 16.6C10.5 16.4 10.5 16.1 10.5 15.8C10.5 15.3 10.4 15 10.1 14.7C9.80001 14.4 9.50001 14.3 9.10001 14.3C9.00001 14.3 8.9 14.3 8.7 14.3C8.5 14.3 8.39999 14.3 8.39999 14.3C8.19999 14.3 7.99999 14.2 7.89999 14.1C7.79999 14 7.7 13.8 7.7 13.7C7.7 13.5 7.79999 13.4 7.89999 13.2C7.99999 13 8.2 13 8.5 13H8.8V13.1ZM15.3 17.5V12.2C14.3 13 13.6 13.3 13.3 13.3C13.1 13.3 13 13.2 12.9 13.1C12.8 13 12.7 12.8 12.7 12.6C12.7 12.4 12.8 12.3 12.9 12.2C13 12.1 13.2 12 13.6 11.8C14.1 11.6 14.5 11.3 14.7 11.1C14.9 10.9 15.2 10.6 15.5 10.3C15.8 10 15.9 9.80003 15.9 9.70003C15.9 9.60003 16.1 9.60004 16.3 9.60004C16.5 9.60004 16.7 9.70003 16.8 9.80003C16.9 9.90003 17 10.2 17 10.5V17.2C17 18 16.7 18.4 16.2 18.4C16 18.4 15.8 18.3 15.6 18.2C15.4 18.1 15.3 17.8 15.3 17.5Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </span>
+                                <input type="date" class="form-control form-control-solid ps-12"
+                                    placeholder="Seleccionar fecha" name="fechaInicio" required />
+                            </div>
+                        </div>
+                        <div class="col-md-6 fv-row">
+                            <label class="required fs-6 fw-bold mb-2">Cliente del proyecto</label>
+                            <select class="form-select form-select-solid" data-placeholder="Seleccionar cliente"
+                                name="cliente" required>
+                                <option value="">Seleccionar cliente</option>
+                                <?php 
+                                while ($dataUsuario2 = mysqli_fetch_array($query2)) { ?>
+                                <?php  if ($dataUsuario2['fechaFin'] != "" and $dataUsuario2['fechaInicio'] != "") { ?>
+                                <option value="<?php echo $dataUsuario2['id']; ?>">
+                                    <?php echo $dataUsuario2['nombres']; ?> <?php echo $dataUsuario2['apellidos']; ?>
+                                </option>
+                                <?php } } ?>
+                            </select>
+                            <label class="required fs-6 fw-bold mb-2">Fecha límite</label>
+                            <div class="position-relative d-flex align-items-center">
+                                <span class="svg-icon svg-icon-2 position-absolute mx-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none">
+                                        <path opacity="0.3"
+                                            d="M21 22H3C2.4 22 2 21.6 2 21V5C2 4.4 2.4 4 3 4H21C21.6 4 22 4.4 22 5V21C22 21.6 21.6 22 21 22Z"
+                                            fill="currentColor" />
+                                        <path
+                                            d="M6 6C5.4 6 5 5.6 5 5V3C5 2.4 5.4 2 6 2C6.6 2 7 2.4 7 3V5C7 5.6 6.6 6 6 6ZM11 5V3C11 2.4 10.6 2 10 2C9.4 2 9 2.4 9 3V5C9 5.6 9.4 6 10 6C10.6 6 11 5.6 11 5ZM15 5V3C15 2.4 14.6 2 14 2C13.4 2 13 2.4 13 3V5C13 5.6 13.4 6 14 6C14.6 6 15 5.6 15 5ZM19 5V3C19 2.4 18.6 2 18 2C17.4 2 17 2.4 17 3V5C17 5.6 17.4 6 18 6C18.6 6 19 5.6 19 5Z"
+                                            fill="currentColor" />
+                                        <path
+                                            d="M8.8 13.1C9.2 13.1 9.5 13 9.7 12.8C9.9 12.6 10.1 12.3 10.1 11.9C10.1 11.6 10 11.3 9.8 11.1C9.6 10.9 9.3 10.8 9 10.8C8.8 10.8 8.59999 10.8 8.39999 10.9C8.19999 11 8.1 11.1 8 11.2C7.9 11.3 7.8 11.4 7.7 11.6C7.6 11.8 7.5 11.9 7.5 12.1C7.5 12.2 7.4 12.2 7.3 12.3C7.2 12.4 7.09999 12.4 6.89999 12.4C6.69999 12.4 6.6 12.3 6.5 12.2C6.4 12.1 6.3 11.9 6.3 11.7C6.3 11.5 6.4 11.3 6.5 11.1C6.6 10.9 6.8 10.7 7 10.5C7.2 10.3 7.49999 10.1 7.89999 10C8.29999 9.90003 8.60001 9.80003 9.10001 9.80003C9.50001 9.80003 9.80001 9.90003 10.1 10C10.4 10.1 10.7 10.3 10.9 10.4C11.1 10.5 11.3 10.8 11.4 11.1C11.5 11.4 11.6 11.6 11.6 11.9C11.6 12.3 11.5 12.6 11.3 12.9C11.1 13.2 10.9 13.5 10.6 13.7C10.9 13.9 11.2 14.1 11.4 14.3C11.6 14.5 11.8 14.7 11.9 15C12 15.3 12.1 15.5 12.1 15.8C12.1 16.2 12 16.5 11.9 16.8C11.8 17.1 11.5 17.4 11.3 17.7C11.1 18 10.7 18.2 10.3 18.3C9.9 18.4 9.5 18.5 9 18.5C8.5 18.5 8.1 18.4 7.7 18.2C7.3 18 7 17.8 6.8 17.6C6.6 17.4 6.4 17.1 6.3 16.8C6.2 16.5 6.10001 16.3 6.10001 16.1C6.10001 15.9 6.2 15.7 6.3 15.6C6.4 15.5 6.6 15.4 6.8 15.4C6.9 15.4 7.00001 15.4 7.10001 15.5C7.20001 15.6 7.3 15.6 7.3 15.7C7.5 16.2 7.7 16.6 8 16.9C8.3 17.2 8.6 17.3 9 17.3C9.2 17.3 9.5 17.2 9.7 17.1C9.9 17 10.1 16.8 10.3 16.6C10.5 16.4 10.5 16.1 10.5 15.8C10.5 15.3 10.4 15 10.1 14.7C9.80001 14.4 9.50001 14.3 9.10001 14.3C9.00001 14.3 8.9 14.3 8.7 14.3C8.5 14.3 8.39999 14.3 8.39999 14.3C8.19999 14.3 7.99999 14.2 7.89999 14.1C7.79999 14 7.7 13.8 7.7 13.7C7.7 13.5 7.79999 13.4 7.89999 13.2C7.99999 13 8.2 13 8.5 13H8.8V13.1ZM15.3 17.5V12.2C14.3 13 13.6 13.3 13.3 13.3C13.1 13.3 13 13.2 12.9 13.1C12.8 13 12.7 12.8 12.7 12.6C12.7 12.4 12.8 12.3 12.9 12.2C13 12.1 13.2 12 13.6 11.8C14.1 11.6 14.5 11.3 14.7 11.1C14.9 10.9 15.2 10.6 15.5 10.3C15.8 10 15.9 9.80003 15.9 9.70003C15.9 9.60003 16.1 9.60004 16.3 9.60004C16.5 9.60004 16.7 9.70003 16.8 9.80003C16.9 9.90003 17 10.2 17 10.5V17.2C17 18 16.7 18.4 16.2 18.4C16 18.4 15.8 18.3 15.6 18.2C15.4 18.1 15.3 17.8 15.3 17.5Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </span>
+                                <input type="date" class="form-control form-control-solid ps-12"
+                                    placeholder="Seleccionar fecha" name="fechaFin" required />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column mb-8">
+                        <label class="fs-6 fw-bold mb-2">Descripción</label>
+                        <textarea class="form-control form-control-solid" rows="3" name="descripcion"
+                            placeholder="Detalles y descripción del proyecto"></textarea>
+                    </div>
+                    <div class="d-flex flex-column mb-8 fv-row">
+                        <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                            <span class="required">Miembros</span>
+                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                title="Specify a target priorty"></i>
+                        </label>
+                        <input class="form-control form-control-solid" name="tags1" />
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-success" name="btnreg" value="regP">
+                            <span class="indicator-label">Guardar</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<?php } else{
+header("location: ../panel/index.php");
+} ?>
---
+++ assets/vista/proyectos/tabla-proyectos2.php
@@ -0,0 +1,134 @@
+<?php
+include('../config.php');
+$sql1 = ("SELECT proyectos.id, proyectos.nombre, proyectos.fechaInicio, proyectos.fechaFin, proyectos.descripcion, proyectos.fechaCreacion, proyectos.estado, proyectosInfo.tipo, proyectosInfo.fechaAdd, proyectosInfo.fechaEstado, proyectosInfo.idProyecto, GROUP_CONCAT(proyectosInfo.idUsuario) AS idUsuarios
+FROM proyectos
+JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto WHERE proyectosInfo.idUsuario = '$session_id'
+GROUP BY proyectos.id");
+$query1 = mysqli_query($con, $sql1);
+$sql2 = ("SELECT proyectos.id, proyectos.nombre, proyectos.fechaInicio, proyectos.fechaFin, proyectos.descripcion, proyectos.fechaCreacion, proyectos.estado, proyectosInfo.tipo, proyectosInfo.fechaAdd, proyectosInfo.fechaEstado, proyectosInfo.idProyecto, GROUP_CONCAT(proyectosInfo.idUsuario) AS idUsuarios
+FROM proyectos
+JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto WHERE proyectosInfo.idUsuario = '$session_id'
+GROUP BY proyectos.id");
+$query2 = mysqli_query($con, $sql2);
+?>
+<div class="content mb-0" id="kt_content">
+    <div class="card mb-0">
+        <div class="card-body py-4 mb-0">
+            <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_table_users">
+                <thead id="kt_table_header">
+                    <tr class="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
+                        <th class="min-w-125px">Nombre</th>
+                        <th class="min-w-125px">Fecha inicio</th>
+                        <th class="min-w-125px">Fecha Límite</th>
+                        <th class="min-w-125px">Descripción</th>
+                        <th class="min-w-125px">Estado</th>
+                    </tr>
+                </thead>
+                <tbody class="text-gray-600 fw-bold">
+                    <?php 
+												$i = 1;
+												while ($dataUsuario1 = mysqli_fetch_array($query1)) { ?>
+                    <tr>
+                        <td><?php echo strtoupper($dataUsuario1['nombre']);?></td>
+                        <td><?php echo $dataUsuario1['fechaInicio']; ?></td>
+                        <td><?php echo $dataUsuario1['fechaFin']; ?></td>
+                        <?php 
+													if ($dataUsuario1['descripcion'] == "") { ?>
+                        <td>-</td>
+                        <?php } else { ?>
+                        <td><?php echo $dataUsuario1['descripcion']; ?></td>
+                        <?php } ?>
+
+                        <?php 
+													if ($dataUsuario1['estado'] == "1") { ?>
+                        <td>En progreso</td>
+                        <?php } else { ?>
+                        <td>Finalizado</td>
+                        <?php } ?>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div class="row g-6 g-xl-9 mt-1" id="card_proyectos">
+<?php 
+    $dataUsuario3Array = array(); // Array para almacenar los datos de $query3
+                        
+    while ($dataUsuario2 = mysqli_fetch_array($query2)) {
+        $idProyecto = $dataUsuario2['id'];
+        include('../config.php');
+        $sql3 = ("SELECT * FROM proyectos JOIN proyectosInfo ON proyectos.id = proyectosInfo.idProyecto LEFT JOIN usuarios ON proyectosInfo.idUsuario = usuarios.id WHERE proyectosInfo.idProyecto = '$idProyecto' AND proyectosInfo.estado='1';");
+        $query3 = mysqli_query($con, $sql3);
+        // Almacenar los datos de $query3 en el array
+        $dataUsuario3Array[$idProyecto] = array();
+        while ($dataUsuario3 = mysqli_fetch_array($query3)) {
+            $dataUsuario3Array[$idProyecto][] = $dataUsuario3;
+        }
+    ?>
+    <div class="col-md-6 col-xl-4 mt-2" style="border-radius: 12px;">
+        <a href="/metronic8/demo14/../demo14/apps/projects/project.html" class="card border-hover-primary">
+            <div class="card-header border-0 pt-9 pb-0">
+                <div class="card-title m-0">
+                    <div class="fs-3 fw-bold text-gray-900">
+                        <?php echo strtoupper($dataUsuario2['nombre']); ?>
+                    </div>
+                </div>
+
+                <div class="card-toolbar">
+                    <span class="badge badge-light-primary fw-bold me-auto px-4 py-3">In Progress</span>
+                </div>
+            </div>
+            <?php 
+                                    $fechaInicio = $dataUsuario2['fechaInicio'];
+                                    $fechaInicioF = date('d/m/Y', strtotime($fechaInicio));
+                                    $fechaFin = $dataUsuario2['fechaFin'];
+                                    $fechaFinF = date('d/m/Y', strtotime($fechaFin));
+                                    $idProyecto = $dataUsuario2['id'];
+                                    ?>
+            <div class="card-body pt-1">
+                <p class="text-gray-500 fw-semibold fs-5 mt-1 mb-7">
+                    <?php echo $dataUsuario2['descripcion']; ?></p>
+                <div class="d-flex flex-wrap mb-5">
+                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-7 mb-3">
+                        <div class="fs-6 text-gray-800 fw-bold"><?php echo $fechaInicioF?></div>
+                        <div class="fw-semibold text-gray-500">Fecha de Inicio</div>
+                    </div>
+                    <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 mb-3">
+                        <div class="fs-6 text-gray-800 fw-bold"><?php echo $fechaFinF?></div>
+                        <div class="fw-semibold text-gray-500">Fecha limite</div>
+                    </div>
+                </div>
+                <div class="h-4px w-100 bg-light mb-5" data-bs-toggle="tooltip" aria-label="This project 50% completed"
+                    data-bs-original-title="This project 50% completed" data-kt-initialized="1">
+                    <div class="bg-primary rounded h-4px" role="progressbar" style="width: 50%" aria-valuenow=" 50"
+                        aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <div class="symbol-group symbol-hover">
+                        <?php 
+                        foreach ($dataUsuario3Array[$idProyecto] as $dataUsuario3) { ?>
+                        <?php if ($dataUsuario3['imagen'] != "blank.png") { ?>
+                            <div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip"
+                                aria-label="<?php echo $dataUsuario3['nombres']; ?> <?php echo $dataUsuario3['apellidos']; ?>"
+                                data-bs-original-title="<?php echo $dataUsuario3['nombres']; ?> <?php echo $dataUsuario3['apellidos']; ?>"
+                                data-kt-initialized="1">
+                                <img alt="<?php echo $dataUsuario3['nombres']; ?> <?php echo $dataUsuario3['apellidos']; ?>"
+                                    src="assets/media/avatars/<?php echo $dataUsuario3['imagen']; ?>">
+                            </div>
+                        <?php } else { ?>
+                            <div class="symbol symbol-35px symbol-circle" data-bs-toggle="tooltip"
+                                aria-label="<?php echo $dataUsuario3['nombres']; ?> <?php echo $dataUsuario3['apellidos']; ?>"
+                                data-bs-original-title="<?php echo $dataUsuario3['nombres']; ?> <?php echo $dataUsuario3['apellidos']; ?>"
+                                data-kt-initialized="1">
+                                <span
+                                    class="symbol-label bg-dark text-inverse-primary fw-bold"><?php echo substr($dataUsuario3['nombres'], 0, 1); ?><?php echo substr($dataUsuario3['apellidos'], 0, 1); ?></span>
+                            </div>
+                        <?php } ?>
+                        <?php } ?>
+                    </div>
+            </div>
+        </a>
+    </div>
+    <?php } ?>
+</div>
