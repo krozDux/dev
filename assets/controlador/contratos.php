a0ceb4b71c79e7e8d191e9e65c91422e20415750
Carlos <1971975680@undc.edu.pe>
2023-11-12 05:02:20 -0500

    wa

--- assets/controlador/contratos.php
+++ assets/controlador/contratos.php
@@ -1,15 +1,6 @@
 <?php
 include('../config.php');
 
-if (!empty($_POST['btneliminar'])) {
-    if (!empty($_POST['id'])) {
-        $id = $_POST['id'];
-        $consulta2 = "UPDATE `usuarios` SET `estado`='2' where id='$id'";
-        $resultado2 = mysqli_query($con, $consulta2);
-        header("location: clientes.php");
-}}
-
-
 if (!empty($_POST['btnreg'])) {
     if (!empty($_POST['regId'])) {
         if (!empty($_POST['regFechaInicio'])) {
