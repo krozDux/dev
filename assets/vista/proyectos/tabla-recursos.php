3b4e930426bbf708a43390f4b252c97daa0bef2d
Carlos <1971975680@undc.edu.pe>
2023-11-11 23:20:35 -0500

    wa

--- assets/vista/proyectos/tabla-recursos.php
+++ assets/vista/proyectos/tabla-recursos.php
@@ -51,7 +51,7 @@ $query1 = mysqli_query($con, $sql1);
                             </div>
                         </div>
                         <div class="d-flex mb-4">
-                        <button class="btn btn-sm btn-bg-light btn-active-color-primary me-3 modal-trabajo">Add User</button>
+                        <button class="btn btn-sm btn-bg-light btn-active-color-primary me-3 modal-trabajo" data-bs-toggle="modal" data-bs-target="#kt_modal_new_user">Add User</button>
                         <a class="btn btn-sm btn-primary me-3" data-bs-toggle="modal" data-bs-target="#kt_modal_new_target">Add Target</a>
                     </div>
                     </div>
