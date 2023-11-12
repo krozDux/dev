54532a54d97e96b000163972445cee544c057dab
Carlos <1971975680@undc.edu.pe>
2023-11-11 22:32:41 -0500

    wa

--- assets/vista/proyectos/tabla-recursos.php
+++ assets/vista/proyectos/tabla-recursos.php
@@ -2436,11 +2436,13 @@ $query1 = mysqli_query($con, $sql1);
                 <div class="card-body d-flex flex-column mb-9 p-9 pt-3">
                     <!--begin::Item-->
                     <div class="d-flex align-items-center position-relative mb-7">
-                        <!--begin::Label-->
-                        <div class="position-absolute top-0 start-0 rounded h-100 bg-secondary w-4px"></div>
-                        <!--end::Label-->
+                        <div class="d-flex flex-stack position-relative mt-8">
+                                <!--begin::Bar-->
+                                <div class="position-absolute h-100 w-4px bg-secondary rounded top-0 start-0"></div>
+                                <!--end::Bar-->
 
-                        <div class="fw-semibold ms-5 text-gray-600">
+                                <!--begin::Info-->
+                                <div class="fw-semibold ms-5 text-gray-600">
                                     <!--begin::Time-->
                                     <div class="fs-5">
                                         14:30 - 15:30
@@ -2461,142 +2463,14 @@ $query1 = mysqli_query($con, $sql1);
                                     </div>
                                     <!--end::User-->
                                 </div>
-                        <!--end::Details-->
+                                <!--end::Info-->
 
-                        <!--begin::Menu-->
-                        <button type="button"
-                            class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                            data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                            <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                    class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                    class="path5"></span></i> </button>
-
-
-
-                        <!--begin::Menu 1-->
-                        <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true"
-                            id="kt_menu_654ebb3d27943">
-                            <!--begin::Header-->
-                            <div class="px-7 py-5">
-                                <div class="fs-5 text-gray-900 fw-bold">Filter Options</div>
+                                <!--begin::Action-->
+                                <a href="#" class="btn btn-bg-light btn-active-color-primary btn-sm">View</a>
+                                <!--end::Action-->
                             </div>
-                            <!--end::Header-->
 
-                            <!--begin::Menu separator-->
-                            <div class="separator border-gray-200"></div>
-                            <!--end::Menu separator-->
-
-
-                            <!--begin::Form-->
-                            <div class="px-7 py-5">
-                                <!--begin::Input group-->
-                                <div class="mb-10">
-                                    <!--begin::Label-->
-                                    <label class="form-label fw-semibold">Status:</label>
-                                    <!--end::Label-->
-
-                                    <!--begin::Input-->
-                                    <div>
-                                        <select class="form-select form-select-solid select2-hidden-accessible"
-                                            multiple="" data-kt-select2="true" data-close-on-select="false"
-                                            data-placeholder="Select option"
-                                            data-dropdown-parent="#kt_menu_654ebb3d27943" data-allow-clear="true"
-                                            data-select2-id="select2-data-23-felo" tabindex="-1" aria-hidden="true"
-                                            data-kt-initialized="1">
-                                            <option></option>
-                                            <option value="1">Approved</option>
-                                            <option value="2">Pending</option>
-                                            <option value="2">In Process</option>
-                                            <option value="2">Rejected</option>
-                                        </select><span class="select2 select2-container select2-container--bootstrap5"
-                                            dir="ltr" data-select2-id="select2-data-24-f7b5" style="width: 100%;"><span
-                                                class="selection"><span
-                                                    class="select2-selection select2-selection--multiple form-select form-select-solid"
-                                                    role="combobox" aria-haspopup="true" aria-expanded="false"
-                                                    tabindex="-1" aria-disabled="false">
-                                                    <ul class="select2-selection__rendered" id="select2-0nj5-container">
-                                                    </ul><span class="select2-search select2-search--inline"><textarea
-                                                            class="select2-search__field" type="search" tabindex="0"
-                                                            autocorrect="off" autocapitalize="none" spellcheck="false"
-                                                            role="searchbox" aria-autocomplete="list" autocomplete="off"
-                                                            aria-label="Search"
-                                                            aria-describedby="select2-0nj5-container"
-                                                            placeholder="Select option"
-                                                            style="width: 100%;"></textarea></span>
-                                                </span></span><span class="dropdown-wrapper"
-                                                aria-hidden="true"></span></span>
-                                    </div>
-                                    <!--end::Input-->
-                                </div>
-                                <!--end::Input group-->
-
-                                <!--begin::Input group-->
-                                <div class="mb-10">
-                                    <!--begin::Label-->
-                                    <label class="form-label fw-semibold">Member Type:</label>
-                                    <!--end::Label-->
-
-                                    <!--begin::Options-->
-                                    <div class="d-flex">
-                                        <!--begin::Options-->
-                                        <label class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                                            <input class="form-check-input" type="checkbox" value="1">
-                                            <span class="form-check-label">
-                                                Author
-                                            </span>
-                                        </label>
-                                        <!--end::Options-->
-
-                                        <!--begin::Options-->
-                                        <label class="form-check form-check-sm form-check-custom form-check-solid">
-                                            <input class="form-check-input" type="checkbox" value="2" checked="checked">
-                                            <span class="form-check-label">
-                                                Customer
-                                            </span>
-                                        </label>
-                                        <!--end::Options-->
-                                    </div>
-                                    <!--end::Options-->
-                                </div>
-                                <!--end::Input group-->
-
-                                <!--begin::Input group-->
-                                <div class="mb-10">
-                                    <!--begin::Label-->
-                                    <label class="form-label fw-semibold">Notifications:</label>
-                                    <!--end::Label-->
-
-                                    <!--begin::Switch-->
-                                    <div
-                                        class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                                        <input class="form-check-input" type="checkbox" value="" name="notifications"
-                                            checked="">
-                                        <label class="form-check-label">
-                                            Enabled
-                                        </label>
-                                    </div>
-                                    <!--end::Switch-->
-                                </div>
-                                <!--end::Input group-->
-
-                                <!--begin::Actions-->
-                                <div class="d-flex justify-content-end">
-                                    <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2"
-                                        data-kt-menu-dismiss="true">Reset</button>
-
-                                    <button type="submit" class="btn btn-sm btn-primary"
-                                        data-kt-menu-dismiss="true">Apply</button>
-                                </div>
-                                <!--end::Actions-->
-                            </div>
-                            <!--end::Form-->
-                        </div>
-                        <!--end::Menu 1-->
-                        <!--end::Menu-->
                     </div>
-                    <!--end::Item-->
-                    <!--begin::Item-->
                     <div class="d-flex align-items-center position-relative mb-7">
                         <!--begin::Label-->
                         <div class="position-absolute top-0 start-0 rounded h-100 bg-secondary w-4px"></div>
