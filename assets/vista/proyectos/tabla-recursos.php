9c8ae2fdcf1e81d8623fee35b1e85f7211c87cb3
Carlos <1971975680@undc.edu.pe>
2023-11-12 19:29:19 -0500

    wa

--- assets/vista/proyectos/tabla-recursos.php
+++ assets/vista/proyectos/tabla-recursos.php
@@ -640,65 +640,43 @@ $query1 = mysqli_query($con, $sql1);
                 ?>
             </div>
         </div>
-
-                <div class="card-body p-9 pt-4">
-                    <ul class="nav nav-pills d-flex flex-nowrap hover-scroll-x py-2" role="tablist">
-                        <?php
-                        setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish_Spain.1252');
-                        for($date = $fechaInicio; $date < $fechaFin; $date->modify('+1 day')) {
-                            $day = $date->format('d'); // Día del mes
-                            // Usar strftime() para obtener la abreviatura del día en español
-                            $dayOfWeek = strftime('%a', $date->getTimestamp()); // Abreviatura del día de la semana en español
-                            $activeClass = $date == $fechaInicio ? 'active' : '';
-                            echo "<li class='nav-item me-1' role='presentation'>
-                                    <a class='nav-link btn d-flex flex-column flex-center rounded-pill min-w-45px me-2 py-4 px-3 btn-active-primary'
-                                        data-bs-toggle='tab' href='#kt_schedule_day_" . $date->format('j') . "' aria-selected='false' tabindex='-1' role='tab'>
-                                        <span class='opacity-50 fs-7 fw-semibold'>$dayOfWeek</span>
-                                        <span class='fs-6 fw-bold'>$day</span>
-                                    </a>
-                                </li>";
-                        }
-                        ?>
-                    </ul>
-                    <?php
-                    } else {
-                        echo "<div class='fs-6 text-gray-500'>Información no disponible.</div>";
-                    }
+        
+        <div class="card-body p-9 pt-4">
+            <ul class="nav nav-pills d-flex flex-nowrap hover-scroll-x py-2" role="tablist">
+                <?php
+                setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish_Spain.1252');
+                foreach ($fechasLimite as $fecha) {
+                    $day = $fecha->format('d'); // Día del mes
+                    $dayOfWeek = strftime('%a', $fecha->getTimestamp()); // Abreviatura del día de la semana en español
+                    $dia = $fecha->format('j'); // 'j' dará el día sin ceros iniciales
+                    echo "<li class='nav-item me-1' role='presentation'>
+                            <a class='nav-link btn d-flex flex-column flex-center rounded-pill min-w-45px me-2 py-4 px-3 btn-active-primary'
+                                data-bs-toggle='tab' href='#kt_schedule_day_" . $dia . "' aria-selected='false' tabindex='-1' role='tab'>
+                                <span class='opacity-50 fs-7 fw-semibold'>$dayOfWeek</span>
+                                <span class='fs-6 fw-bold'>$day</span>
+                            </a>
+                          </li>";
+                }
+                ?>
+            </ul>
+            <?php
+            } else {
+                echo "<div class='fs-6 text-gray-500'>Información no disponible.</div>";
+            }
+            ?>
+            <div class="tab-content">
+                <?php foreach ($fechasLimite as $fecha): ?>
+                    <?php 
+                    $dia = $fecha->format('j'); // 'j' dará el día sin ceros iniciales
                     ?>
-                    <div class="tab-content">
-                        <?php 
-                        $sqlTareas = "SELECT * FROM proyectosTareas WHERE idProyecto = '$idProyecto'";
-                        $queryTareas = mysqli_query($con, $sqlTareas);
-                        $tareas = mysqli_fetch_all($queryTareas, MYSQLI_ASSOC);
-                        foreach ($tareas as $tarea): ?>
-                            <?php 
-                            // Convertimos la fechaFin a un objeto DateTime
-                            $fechaFin = new DateTime($tarea['fechaFin']);
-                            // Extraemos el día del objeto DateTime
-                            $dia = $fechaFin->format('j'); // 'j' dará el día sin ceros iniciales
-                            ?>
-                            <div id="kt_schedule_day_<?php echo $dia; ?>" class="tab-pane fade show" role="tabpanel">
-                                <div class="d-flex flex-stack position-relative mt-8">
-                                    <!-- Tu contenido para cada tarea aquí -->
-                                    <div class="position-absolute h-100 w-4px bg-secondary rounded top-0 start-0"></div>
-                                    <div class="fw-semibold ms-5 text-gray-600">
-                                        <!-- Descripción de la tarea -->
-                                        <a href="#" class="fs-5 fw-bold text-gray-800 text-hover-primary mb-2">
-                                            <?php echo $tarea['nombre']; ?>
-                                        </a>
-                                        <!-- Más información de la tarea -->
-                                        <div class="text-gray-500">
-                                            Lead by <a href="#"><?php echo $tarea['lider']; ?></a> <!-- Ajustar según sea necesario -->
-                                        </div>
-                                    </div>
-                                    <a href="#" class="btn btn-bg-light btn-active-color-primary btn-sm">View</a>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
+                    <div id="kt_schedule_day_<?php echo $dia; ?>" class="tab-pane fade show" role="tabpanel">
+                        <!-- Aquí puedes poner el contenido para cada día de tareas -->
                     </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
+    </div>
+</div>
 
         <div class="col-lg-6">
 
