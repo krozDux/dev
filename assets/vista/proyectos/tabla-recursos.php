71a490d1202037676be3387ab15ff6ffbe328864
Carlos <1971975680@undc.edu.pe>
2023-11-13 18:18:41 -0500

    wa

--- assets/vista/proyectos/tabla-recursos.php
+++ assets/vista/proyectos/tabla-recursos.php
@@ -362,179 +362,38 @@ $query1 = mysqli_query($con, $sql1);
                 <div class="card-header mt-6">
                     <!--begin::Card title-->
                     <div class="card-title flex-column">
-                        <h3 class="fw-bold mb-1">New Contibutors</h3>
+                        <h3 class="fw-bold mb-1">Contribuidores</h3>
 
-                        <div class="fs-6 text-gray-500">From total 482 Participants</div>
+                        <div class="fs-6 text-gray-500">el proyecto consta de CONTRIBUIDORES miembros en total</div>
                     </div>
-                    <!--end::Card title-->
-
-                    <!--begin::Card toolbar-->
                     <div class="card-toolbar">
                         <a href="#" class="btn btn-bg-light btn-active-color-primary btn-sm">View All</a>
                     </div>
-                    <!--end::Card toolbar-->
                 </div>
-                <!--end::Card toolbar-->
 
-                <!--begin::Card body-->
                 <div class="card-body d-flex flex-column p-9 pt-3 ">
 
-                    <!--begin::Item-->
                     <div class="d-flex align-items-center mb-5">
-                        <!--begin::Avatar-->
                         <div class="me-5 position-relative">
-                            <!--begin::Image-->
                             <div class="symbol symbol-35px symbol-circle">
                                 <img alt="Pic" src="/assets/media/avatars/300-6.jpg">
                             </div>
-                            <!--end::Image-->
 
                         </div>
-                        <!--end::Avatar-->
 
-                        <!--begin::Details-->
                         <div class="fw-semibold">
                             <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary">Emma Smith</a>
-
                             <div class="text-gray-500">
                                 8 Pending &amp; 97 Completed Tasks </div>
                         </div>
-                        <!--end::Details-->
 
-                        <!--begin::Badge-->
                         <div class="badge badge-light ms-auto">5</div>
-                        <!--end::Badge-->
                     </div>
-                    <!--end::Item-->
-
-                    <!--begin::Item-->
-                    <div class="d-flex align-items-center mb-5">
-                        <!--begin::Avatar-->
-                        <div class="me-5 position-relative">
-                            <!--begin::Image-->
-                            <div class="symbol symbol-35px symbol-circle">
-                                <span class="symbol-label bg-light-danger text-danger fw-semibold">
-                                    M </span>
-                            </div>
-                            <!--end::Image-->
-
-                            <!--begin::Online-->
-                            <div
-                                class="bg-success position-absolute h-8px w-8px rounded-circle translate-middle start-100 top-100 ms-n1 mt-n1">
-                            </div>
-                            <!--end::Online-->
-                        </div>
-                        <!--end::Avatar-->
-
-                        <!--begin::Details-->
-                        <div class="fw-semibold">
-                            <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary">Melody Macy</a>
-
-                            <div class="text-gray-500">
-                                5 Pending &amp; 84 Completed </div>
-                        </div>
-                        <!--end::Details-->
-
-                        <!--begin::Badge-->
-                        <div class="badge badge-light ms-auto">8</div>
-                        <!--end::Badge-->
-                    </div>
-                    <!--end::Item-->
-
-                    <!--begin::Item-->
-                    <div class="d-flex align-items-center mb-5">
-                        <!--begin::Avatar-->
-                        <div class="me-5 position-relative">
-                            <!--begin::Image-->
-                            <div class="symbol symbol-35px symbol-circle">
-                                <img alt="Pic" src="/assets/media/avatars/300-1.jpg">
-                            </div>
-                            <!--end::Image-->
-
-                        </div>
-                        <!--end::Avatar-->
-
-                        <!--begin::Details-->
-                        <div class="fw-semibold">
-                            <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary">Max Smith</a>
-
-                            <div class="text-gray-500">
-                                9 Pending &amp; 103 Completed </div>
-                        </div>
-                        <!--end::Details-->
-
-                        <!--begin::Badge-->
-                        <div class="badge badge-light ms-auto">9</div>
-                        <!--end::Badge-->
-                    </div>
-                    <!--end::Item-->
-
-                    <!--begin::Item-->
-                    <div class="d-flex align-items-center mb-5">
-                        <!--begin::Avatar-->
-                        <div class="me-5 position-relative">
-                            <!--begin::Image-->
-                            <div class="symbol symbol-35px symbol-circle">
-                                <img alt="Pic" src="/assets/media/avatars/300-5.jpg">
-                            </div>
-                            <!--end::Image-->
-
-                        </div>
-                        <!--end::Avatar-->
-
-                        <!--begin::Details-->
-                        <div class="fw-semibold">
-                            <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary">Sean Bean</a>
-
-                            <div class="text-gray-500">
-                                3 Pending &amp; 55 Completed </div>
-                        </div>
-                        <!--end::Details-->
-
-                        <!--begin::Badge-->
-                        <div class="badge badge-light ms-auto">3</div>
-                        <!--end::Badge-->
-                    </div>
-                    <!--end::Item-->
-
-                    <!--begin::Item-->
-                    <div class="d-flex align-items-center ">
-                        <!--begin::Avatar-->
-                        <div class="me-5 position-relative">
-                            <!--begin::Image-->
-                            <div class="symbol symbol-35px symbol-circle">
-                                <img alt="Pic" src="/assets/media/avatars/300-25.jpg">
-                            </div>
-                            <!--end::Image-->
-
-                        </div>
-                        <!--end::Avatar-->
-
-                        <!--begin::Details-->
-                        <div class="fw-semibold">
-                            <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary">Brian Cox</a>
-
-                            <div class="text-gray-500">
-                                4 Pending &amp; 115 Completed </div>
-                        </div>
-                        <!--end::Details-->
-
-                        <!--begin::Badge-->
-                        <div class="badge badge-light ms-auto">4</div>
-                        <!--end::Badge-->
-                    </div>
-                    <!--end::Item-->
                 </div>
-                <!--end::Card body-->
             </div>
-            <!--end::Card-->
         </div>
-        <!--end::Col-->
         <div class="col-lg-6 mb-6">
-
-            <!--begin::Card-->
-            <div class="card card-flush h-lg-100">
-                <!--begin::Card header-->
+            <div class="card card-flush h-lg-60">
                 <div class="card-header mt-6">
                     <!--begin::Card title-->
                     <div class="card-title flex-column">
@@ -555,7 +414,7 @@ $query1 = mysqli_query($con, $sql1);
                 <!--begin::Card body-->
                 <div class="card-body p-9 pt-3">
                     <!--begin::Files-->
-                    <div class="d-flex flex-column mb-9">
+                    <div class="d-flex flex-column">
                         <!--begin::File-->
                         <div class="d-flex align-items-center mb-5">
                             <!--begin::Icon-->
