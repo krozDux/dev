d579e3e8bba8b0c94935b6a6756f40dbfea49201
Carlos <1971975680@undc.edu.pe>
2023-11-20 00:06:18 -0500

    wa

--- ejemplo.php
+++ ejemplo.php
@@ -1,4 +1,8 @@
 <?php 
+require_once 'vendor/autoload.php';
+
+use PhpOffice\PhpWord\TemplateProcessor;
+
 
 // Enviar el documento al navegador para la descarga
 header('Content-Disposition: attachment; filename=documento-final.docx');
