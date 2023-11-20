dbb776ed108e41dcfdff51028171813c77774381
Carlos <1971975680@undc.edu.pe>
2023-11-20 00:05:59 -0500

    wa

--- ejemplo.php
+++ ejemplo.php
@@ -1,21 +1,4 @@
 <?php 
-require_once 'vendor/autoload.php';
-
-use PhpOffice\PhpWord\TemplateProcessor;
-
-// Ruta a la plantilla de Word
-$templateProcessor = new TemplateProcessor('/nombre.docx');
-
-// Recibir nombre y apellido por algún método, por ejemplo, POST
-$nombre = 'hola';
-$apellido = 'hola';
-
-// Reemplazar los marcadores de posición en la plantilla
-$templateProcessor->setValue('nombre', $nombre);
-$templateProcessor->setValue('apellido', $apellido);
-
-// Guardar el documento resultante
-$templateProcessor->saveAs('documento-final.docx');
 
 // Enviar el documento al navegador para la descarga
 header('Content-Disposition: attachment; filename=documento-final.docx');
