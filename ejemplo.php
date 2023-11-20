0a9ca4352e4ae1e3d1f6f6e5adb1e7618a574a1d
Carlos <1971975680@undc.edu.pe>
2023-11-20 00:14:14 -0500

    wa

--- ejemplo.php
+++ ejemplo.php
@@ -3,31 +3,32 @@ require_once 'vendor/autoload.php';
 
 use PhpOffice\PhpWord\TemplateProcessor;
 
-try {
-    // Ruta a la plantilla de Word
-    $templateProcessor = new TemplateProcessor('nombre.docx');
+$templateProcessor = new TemplateProcessor('nombre.docx');
 
-    // Recibir nombre y apellido por algún método, por ejemplo, POST
-    $nombre = 'hola'; // Aquí deberías usar $_POST['nombre'] si esperas un POST
-    $apellido = 'asd'; // Y $_POST['apellido'] para el apellido
+$nombre = 'hola'; // Debería ser $_POST['nombre'] en producción
+$apellido = 'asd'; // Debería ser $_POST['apellido'] en producción
 
-    // Reemplazar los marcadores de posición en la plantilla
-    $templateProcessor->setValue('nombre', $nombre);
-    $templateProcessor->setValue('apellido', $apellido);
+// Depuración: Verificar los valores antes de la sustitución
+var_dump($nombre);
+var_dump($apellido);
 
-    // Guardar el documento resultante
-    $documentPath = 'documento-final.docx';
-    $templateProcessor->saveAs($documentPath);
+// Reemplazar los marcadores de posición en la plantilla
+$templateProcessor->setValue('nombre', $nombre);
+$templateProcessor->setValue('apellido', $apellido);
 
-    // Enviar el documento al navegador para la descarga
-    header('Content-Disposition: attachment; filename=' . basename($documentPath));
-    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-    header('Content-Length: ' . filesize($documentPath));
-    header('Cache-Control: max-age=0');
-    readfile($documentPath);
-    exit;
-} catch (Exception $e) {
-    // Manejo de la excepción
-    echo 'Error: ' . $e->getMessage();
-}
+// Guardar el documento resultante
+$documentPath = 'documento-final.docx';
+$templateProcessor->saveAs($documentPath);
+
+// Depuración: Verificar si el documento tiene los valores sustituidos
+$templateProcessor = new TemplateProcessor($documentPath);
+$text = $templateProcessor->getVariables();
+var_dump($text);
+
+// Enviar el documento al navegador para la descarga
+header('Content-Disposition: attachment; filename=' . basename($documentPath));
+header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+header('Content-Length: ' . filesize($documentPath));
+header('Cache-Control: max-age=0');
+readfile($documentPath);
 ?>
