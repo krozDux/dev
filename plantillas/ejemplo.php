c1e887bc2a552931414b7fc5bdde623f65247d0f
Carlos <1971975680@undc.edu.pe>
2023-11-20 00:01:44 -0500

    wa

---
+++ plantillas/ejemplo.php
@@ -0,0 +1,25 @@
+<?php 
+require_once '../vendor/autoload.php';
+
+use PhpOffice\PhpWord\TemplateProcessor;
+
+// Ruta a la plantilla de Word
+$templateProcessor = new TemplateProcessor('/plantilla.docx');
+
+// Recibir nombre y apellido por algún método, por ejemplo, POST
+// $nombre = $_POST['nombre'];
+// $apellido = $_POST['apellido'];
+$nombre = 'Carlos';
+$apellido = 'Andres';
+
+// Reemplazar los marcadores de posición en la plantilla
+$templateProcessor->setValue('nombre', $nombre);
+$templateProcessor->setValue('apellido', $apellido);
+
+// Guardar el documento resultante
+$templateProcessor->saveAs('documento-final.docx');
+
+// Enviar el documento al navegador para la descarga
+header('Content-Disposition: attachment; filename=documento-final.docx');
+readfile('documento-final.docx');
+?>
