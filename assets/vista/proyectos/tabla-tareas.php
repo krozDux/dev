8c447af60f34fc170565dbb3a25da52826110435
Carlos <1971975680@undc.edu.pe>
2023-11-21 14:10:49 -0500

    wa

--- assets/vista/proyectos/tabla-tareas.php
+++ assets/vista/proyectos/tabla-tareas.php
@@ -303,82 +303,17 @@ $query1 = mysqli_query($con, $sql1);
 																			</g>
 																		</svg>
 																	</span>
-																	<!--end::Svg Icon-->
 																</button>
-																<!--begin::Menu 3-->
 																<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-200px py-3" data-kt-menu="true">
-																	<!--begin::Heading-->
 																	<div class="menu-item px-3">
 																		<div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">Payments</div>
 																	</div>
-																	<!--end::Heading-->
-																	<!--begin::Menu item-->
 																	<div class="menu-item px-3">
 																		<a href="#" class="menu-link px-3">Create Invoice</a>
 																	</div>
-																	<!--end::Menu item-->
-																	<!--begin::Menu item-->
-																	<div class="menu-item px-3">
-																		<a href="#" class="menu-link flex-stack px-3">Create Payment
-																		<i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Specify a target name for future usage and reference"></i></a>
-																	</div>
-																	<!--end::Menu item-->
-																	<!--begin::Menu item-->
-																	<div class="menu-item px-3">
-																		<a href="#" class="menu-link px-3">Generate Bill</a>
-																	</div>
-																	<!--end::Menu item-->
-																	<!--begin::Menu item-->
-																	<div class="menu-item px-3" data-kt-menu-trigger="hover" data-kt-menu-placement="right-end">
-																		<a href="#" class="menu-link px-3">
-																			<span class="menu-title">Subscription</span>
-																			<span class="menu-arrow"></span>
-																		</a>
-																		<!--begin::Menu sub-->
-																		<div class="menu-sub menu-sub-dropdown w-175px py-4">
-																			<!--begin::Menu item-->
-																			<div class="menu-item px-3">
-																				<a href="#" class="menu-link px-3">Plans</a>
-																			</div>
-																			<!--end::Menu item-->
-																			<!--begin::Menu item-->
-																			<div class="menu-item px-3">
-																				<a href="#" class="menu-link px-3">Billing</a>
-																			</div>
-																			<!--end::Menu item-->
-																			<!--begin::Menu item-->
-																			<div class="menu-item px-3">
-																				<a href="#" class="menu-link px-3">Statements</a>
-																			</div>
-																			<!--end::Menu item-->
-																			<!--begin::Menu separator-->
-																			<div class="separator my-2"></div>
-																			<!--end::Menu separator-->
-																			<!--begin::Menu item-->
-																			<div class="menu-item px-3">
-																				<div class="menu-content px-3">
-																					<!--begin::Switch-->
-																					<label class="form-check form-switch form-check-custom form-check-solid">
-																						<!--begin::Input-->
-																						<input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications">
-																						<!--end::Input-->
-																						<!--end::Label-->
-																						<span class="form-check-label text-muted fs-6">Recuring</span>
-																						<!--end::Label-->
-																					</label>
-																					<!--end::Switch-->
-																				</div>
-																			</div>
-																			<!--end::Menu item-->
-																		</div>
-																		<!--end::Menu sub-->
-																	</div>
-																	<!--end::Menu item-->
-																	<!--begin::Menu item-->
 																	<div class="menu-item px-3 my-1">
 																		<a href="#" class="menu-link px-3">Settings</a>
 																	</div>
-																	<!--end::Menu item-->
 																</div>
 																<!--end::Menu 3-->
 															</div>
@@ -2081,9 +2016,6 @@ $query1 = mysqli_query($con, $sql1);
 															</td>
 															<td>
 																<span class="badge badge-light fw-bolder me-auto">Yet to start</span>
-															</td>
-															<td class="text-end">
-																<a href="#" class="btn btn-bg-light btn-active-color-primary btn-sm">View</a>
 															</td>
 														</tr>
 														<!--end::Table row-->
