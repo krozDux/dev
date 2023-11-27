34258198c40ff64fdba333ff704cde71a9f368b0
Carlos <1971975680@undc.edu.pe>
2023-11-27 00:09:02 -0500

    wa

--- assets/vista/proyectos/tabla-tareas.php
+++ assets/vista/proyectos/tabla-tareas.php
@@ -298,11 +298,14 @@ $query1 = mysqli_query($con, $sql1);
                                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-200px py-3"
                                         data-kt-menu="true">
                                         <div class="menu-item px-3">
-                                            <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">Opciones
+                                            <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">Payments
                                             </div>
                                         </div>
                                         <div class="menu-item px-3">
-                                            <a href="#" class="menu-link px-3">Agregar documento</a>
+                                            <a href="#" class="menu-link px-3">Create Invoice</a>
+                                        </div>
+                                        <div class="menu-item px-3 my-1">
+                                            <a href="#" class="menu-link px-3">Settings</a>
                                         </div>
                                     </div>
                                 </div>
@@ -322,7 +325,7 @@ $query1 = mysqli_query($con, $sql1);
                                                     fill="currentColor"></path>
                                             </svg>
                                         </span>
-                                        <span class="ms-1 fs-7 fw-bolder text-gray-600">CANTIDAD DE ARCHIVOS SUBIDOS</span>
+                                        <span class="ms-1 fs-7 fw-bolder text-gray-600">CANTIDAD DE ARCHIVOS</span>
                                     </div>
                                     
                                 </div>
