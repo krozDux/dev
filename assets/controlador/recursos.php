ec84d2ae1125834a0416c68970735e003065239b
Carlos <1971975680@undc.edu.pe>
2023-12-01 00:59:03 -0500

    wa

--- assets/controlador/recursos.php
+++ assets/controlador/recursos.php
@@ -127,4 +127,51 @@ if (!empty($_POST['btnregTarea'])) {
         
     }
 }
+
+
+
+if (!empty($_POST['btnregMiembro'])) {
+    if (!empty($_POST['idProyecto'])) {
+        $idProyecto = $_POST['idProyecto']; // Asegúrate de asignar esta variable correctamente
+        $fechaAdd = date('Y-m-d H:i:s');
+        $jsonData = $_POST['tags1'];
+        $data = json_decode($jsonData);
+
+        if ($data) {
+            foreach ($data as $item) {
+                $matches = [];
+                preg_match('/\[(\d+)\]/', $item->value, $matches);
+
+                if (isset($matches[1])) {
+                    $value = $matches[1];
+                    
+                    // Verifica si ya existe un registro con el idUsuario e idProyecto
+                    $consultaVerificacion = "SELECT * FROM `proyectosInfo` WHERE `idUsuario` = '$value' AND `idProyecto` = '$idProyecto'";
+                    $resultadoVerificacion = mysqli_query($con, $consultaVerificacion);
+                    
+                    if (mysqli_num_rows($resultadoVerificacion) < 0) {
+                        // Si no existe, inserta el valor en la tabla MySQL
+                        $consulta7 = "INSERT INTO `proyectosInfo` (`tipo`,`estado`,`fechaAdd`,`idUsuario`,`idProyecto`) VALUES ('1','1','$fechaAdd','$value','$idProyecto')";
+                        $resultado7 = mysqli_query($con, $consulta7);
+                        if (!$resultado7) {
+                            echo "Error al insertar el valor '$value'. Error: " . mysqli_error($con);
+                        }
+                    }
+                }
+            }
+        } 
+        header("location: miembros.php?idProyecto=$idProyecto");
+    } else {
+        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 bg-danger" role="alert" aria-live="assertive" aria-atomic="true" style="z-index: 1050;">
+            <div class="toast-header bg-danger">
+                <i class="ki-duotone ki-abstract-39 fs-2 bg-danger"><span class="path1 bg-danger"></span><span class="path2 bg-danger"></span></i>
+                <strong class="me-auto text-white">Alerta</strong>
+                <button type="button" class="btn-close bg-white" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body text-white">
+                Tiene que existir un identificador del proyecto.
+            </div>
+        </div>';
+    }
+}
 ?>
