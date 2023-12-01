6b2adfc0a7da59ec5852b30c0ae6b13d99443d6e
Carlos <1971975680@undc.edu.pe>
2023-12-01 01:33:31 -0500

    wa

--- assets/controlador/recursos.php
+++ assets/controlador/recursos.php
@@ -150,16 +150,6 @@ if (!empty($_POST['btnregMiembro'])) {
                     $resultadoVerificacion = mysqli_query($con, $consultaVerificacion);
                     
                     if (mysqli_num_rows($resultadoVerificacion) > 0) {
-                        echo '<div class="toast show position-fixed bottom-0 end-0 p-2 bg-danger" role="alert" aria-live="assertive" aria-atomic="true" style="z-index: 1050;">
-                        <div class="toast-header bg-danger">
-                            <i class="ki-duotone ki-abstract-39 fs-2 bg-danger"><span class="path1 bg-danger"></span><span class="path2 bg-danger"></span></i>
-                            <strong class="me-auto text-white">Alerta</strong>
-                            <button type="button" class="btn-close bg-white" data-bs-dismiss="toast" aria-label="Close"></button>
-                        </div>
-                        <div class="toast-body text-white">
-                            ya hay con ese id
-                        </div>
-                    </div>';
                     } else {
                         // Si no existe, inserta el valor en la tabla MySQL
                         $consulta7 = "INSERT INTO `proyectosInfo` (`tipo`,`estado`,`fechaAdd`,`idUsuario`,`idProyecto`) VALUES ('1','1','$fechaAdd','$value','$idProyecto')";
