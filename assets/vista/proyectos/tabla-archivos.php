c64d2320473555064ac5de44f73c8240f63d3d88
Carlos <1971975680@undc.edu.pe>
2023-12-09 05:13:53 -0500

    wa

--- assets/vista/proyectos/tabla-archivos.php
+++ assets/vista/proyectos/tabla-archivos.php
@@ -303,7 +303,7 @@ $query1 = mysqli_query($con, $sql1);
                                                                 class="path1"></span><span class="path2"></span><span
                                                                 class="path3"></span><span class="path4"></span></i>
                                                     </button>
-                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
+                                                    <div class="menu menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
                                                         data-kt-menu="true">
                                                         <div class="menu-item px-3">
                                                             <a href="/metronic8/demo14/?page=apps/file-manager/files"
