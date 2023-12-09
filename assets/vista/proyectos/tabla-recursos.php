b8215836986bbe8631b91a5276b5f2fc7b6f2094
Carlos <1971975680@undc.edu.pe>
2023-12-09 02:43:17 -0500

    wa

--- assets/vista/proyectos/tabla-recursos.php
+++ assets/vista/proyectos/tabla-recursos.php
@@ -469,14 +469,13 @@ $query1 = mysqli_query($con, $sql1);
                 <div class="card-header mt-6">
                 <?php
                         include('../config.php');
-                        $sqlArchivos = ("SELECT * FROM `proyectosTareas` INNER JOIN proyectosDocumentos ON proyectosTareas.id = proyectosDocumentos.idProyectoTarea  WHERE proyectosTareas.idProyecto = '$idProyecto';");
+                        $sqlArchivos = ("SELECT proyectosTareas.nombre,proyectosDocumentos.fechaAdd,proyectosDocumentos.documento,proyectosDocumentos.extension,proyectosDocumentos.link FROM `proyectosTareas` INNER JOIN proyectosDocumentos ON proyectosTareas.id = proyectosDocumentos.idProyectoTarea  WHERE proyectosTareas.idProyecto = '$idProyecto';");
                         $queryArchivos = mysqli_query($con, $sqlArchivos);
                         if ($queryArchivos) {
                             $totalArchivos = mysqli_num_rows($queryArchivos); // Contamos el total de registros
                         }?>
                     <div class="card-title flex-column">
                         <h3 class="fw-bold mb-1">Archivos subidos</h3>
-
                         <div class="fs-6 text-gray-500">Hay un total de <?php echo $totalArchivos; ?> archivos.</div>
                     </div>
 
@@ -485,239 +484,20 @@ $query1 = mysqli_query($con, $sql1);
                     </div>
                 </div>
                 <div class="card-body p-9 pt-3">
-                    <!--begin::Files-->
                     <div class="d-flex flex-column">
-                        <!--begin::File-->
-                        <div class="d-flex align-items-center mb-5">
-                            <!--begin::Icon-->
-                            <div class="symbol symbol-30px me-5">
-                                <img alt="Icon" src="/assets/media/svg/files/pdf.svg">
-                            </div>
-                            <!--end::Icon-->
-
-                            <!--begin::Details-->
-                            <div class="fw-semibold">
-                                <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#">Project tech
-                                    requirements</a>
-
-                                <div class="text-gray-500">
-                                    2 days ago <a href="#">Karina Clark</a>
+                        <?php while ($DataArchivo = mysqli_fetch_assoc($queryArchivos)) {    ?>
+                            <div class="d-flex align-items-center mb-5">
+                                <div class="symbol symbol-30px me-5">
+                                    <img alt="Icon" src="/assets/media/svg/files/pdf.svg">
                                 </div>
-                            </div>
-                            <!--end::Details-->
-
-                            <!--begin::Menu-->
-                            <button type="button"
-                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                                <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                        class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                        class="path5"></span></i> </button>
-
-
-
-                            <!--begin::Menu 1-->
-                            <div class="menu menu-sub menu-sub-dropdown w-250px w-md-300px" data-kt-menu="true"
-                                id="kt_menu_654ebb3d27871">
-                                <!--begin::Header-->
-                                <div class="px-7 py-5">
-                                    <div class="fs-5 text-gray-900 fw-bold">Filter Options</div>
-                                </div>
-                                <!--end::Header-->
-
-                                <!--begin::Menu separator-->
-                                <div class="separator border-gray-200"></div>
-                                <!--end::Menu separator-->
-
-
-                                <!--begin::Form-->
-                                <div class="px-7 py-5">
-                                    <!--begin::Input group-->
-                                    <div class="mb-10">
-                                        <!--begin::Label-->
-                                        <label class="form-label fw-semibold">Status:</label>
-                                        <!--end::Label-->
-
-                                        <!--begin::Input-->
-                                        <div>
-                                            <select class="form-select form-select-solid select2-hidden-accessible"
-                                                multiple="" data-kt-select2="true" data-close-on-select="false"
-                                                data-placeholder="Select option"
-                                                data-dropdown-parent="#kt_menu_654ebb3d27871" data-allow-clear="true"
-                                                data-select2-id="select2-data-15-qyzp" tabindex="-1" aria-hidden="true"
-                                                data-kt-initialized="1">
-                                                <option></option>
-                                                <option value="1">Approved</option>
-                                                <option value="2">Pending</option>
-                                                <option value="2">In Process</option>
-                                                <option value="2">Rejected</option>
-                                            </select><span
-                                                class="select2 select2-container select2-container--bootstrap5"
-                                                dir="ltr" data-select2-id="select2-data-16-63et"
-                                                style="width: 100%;"><span class="selection"><span
-                                                        class="select2-selection select2-selection--multiple form-select form-select-solid"
-                                                        role="combobox" aria-haspopup="true" aria-expanded="false"
-                                                        tabindex="-1" aria-disabled="false">
-                                                        <ul class="select2-selection__rendered"
-                                                            id="select2-z37m-container"></ul><span
-                                                            class="select2-search select2-search--inline"><textarea
-                                                                class="select2-search__field" type="search" tabindex="0"
-                                                                autocorrect="off" autocapitalize="none"
-                                                                spellcheck="false" role="searchbox"
-                                                                aria-autocomplete="list" autocomplete="off"
-                                                                aria-label="Search"
-                                                                aria-describedby="select2-z37m-container"
-                                                                placeholder="Select option"
-                                                                style="width: 100%;"></textarea></span>
-                                                    </span></span><span class="dropdown-wrapper"
-                                                    aria-hidden="true"></span></span>
-                                        </div>
-                                        <!--end::Input-->
-                                    </div>
-                                    <!--end::Input group-->
-
-                                    <!--begin::Input group-->
-                                    <div class="mb-10">
-                                        <!--begin::Label-->
-                                        <label class="form-label fw-semibold">Member Type:</label>
-                                        <!--end::Label-->
-
-                                        <!--begin::Options-->
-                                        <div class="d-flex">
-                                            <!--begin::Options-->
-                                            <label
-                                                class="form-check form-check-sm form-check-custom form-check-solid me-5">
-                                                <input class="form-check-input" type="checkbox" value="1">
-                                                <span class="form-check-label">
-                                                    Author
-                                                </span>
-                                            </label>
-                                            <!--end::Options-->
-
-                                            <!--begin::Options-->
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid">
-                                                <input class="form-check-input" type="checkbox" value="2"
-                                                    checked="checked">
-                                                <span class="form-check-label">
-                                                    Customer
-                                                </span>
-                                            </label>
-                                            <!--end::Options-->
-                                        </div>
-                                        <!--end::Options-->
-                                    </div>
-                                    <!--end::Input group-->
-
-                                    <!--begin::Input group-->
-                                    <div class="mb-10">
-                                        <!--begin::Label-->
-                                        <label class="form-label fw-semibold">Notifications:</label>
-                                        <!--end::Label-->
-
-                                        <!--begin::Switch-->
-                                        <div
-                                            class="form-check form-switch form-switch-sm form-check-custom form-check-solid">
-                                            <input class="form-check-input" type="checkbox" value=""
-                                                name="notifications" checked="">
-                                            <label class="form-check-label">
-                                                Enabled
-                                            </label>
-                                        </div>
-                                        <!--end::Switch-->
-                                    </div>
-                                    <!--end::Input group-->
-
-                                    <!--begin::Actions-->
-                                    <div class="d-flex justify-content-end">
-                                        <button type="reset" class="btn btn-sm btn-light btn-active-light-primary me-2"
-                                            data-kt-menu-dismiss="true">Reset</button>
-
-                                        <button type="submit" class="btn btn-sm btn-primary"
-                                            data-kt-menu-dismiss="true">Apply</button>
+                                <div class="fw-semibold">
+                                    <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#"><?php echo $DataArchivo['nombre']; ?></a>
+                                    <div class="text-gray-500">
+                                    <?php echo $DataArchivo['fecha']; ?> <a href="#">Karina Clark</a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="d-flex align-items-center mb-5">
-                            <div class="symbol symbol-30px me-5">
-                                <img alt="Icon" src="/assets/media/svg/files/doc.svg">
-                            </div>
-                            <div class="fw-semibold">
-                                <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#">Create FureStibe
-                                    branding proposal</a>
-
-                                <div class="text-gray-500">
-                                    Due in 1 day <a href="#">Marcus Blake</a>
-                                </div>
-                            </div>
-                            <button type="button"
-                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                                <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                        class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                        class="path5"></span></i> </button>
-                        </div>
-                        <div class="d-flex align-items-center mb-5">
-                            <div class="symbol symbol-30px me-5">
-                                <img alt="Icon" src="/assets/media/svg/files/css.svg">
-                            </div>
-                            <div class="fw-semibold">
-                                <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#">Completed Project
-                                    Stylings</a>
-                                <div class="text-gray-500">
-                                    Due in 1 day <a href="#">Terry Barry</a>
-                                </div>
-                            </div>
-                            <button type="button"
-                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                                <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                        class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                        class="path5"></span></i> </button>
-                        </div>
-                        <div class="d-flex align-items-center mb-5">
-                            <div class="symbol symbol-30px me-5">
-                                <img alt="Icon" src="/assets/media/svg/files/ai.svg">
-                            </div>
-                            <div class="fw-semibold">
-                                <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#">Create Project
-                                    Wireframes</a>
-
-                                <div class="text-gray-500">
-                                    Due in 3 days <a href="#">Roth Bloom</a>
-                                </div>
-                            </div>
-                            <button type="button"
-                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                                <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                        class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                        class="path5"></span></i> </button>
-                        </div>
-                        <div class="d-flex align-items-center mb-5">
-                            <div class="symbol symbol-30px me-5">
-                                <img alt="Icon" src="/assets/media/svg/files/ai.svg">
-                            </div>
-                            <div class="fw-semibold">
-                                <a class="fs-6 fw-bold text-gray-900 text-hover-primary" href="#">Create Project
-                                    Wireframes</a>
-
-                                <div class="text-gray-500">
-                                    Due in 3 days <a href="#">Roth Bloom</a>
-                                </div>
-                            </div>
-                            <button type="button"
-                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary ms-auto"
-                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-
-                                <i class="ki-duotone ki-element-plus fs-3"><span class="path1"></span><span
-                                        class="path2"></span><span class="path3"></span><span class="path4"></span><span
-                                        class="path5"></span></i> </button>
-                        </div>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
