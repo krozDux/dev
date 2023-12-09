d429a33bcb2e6458a8a32b42330dff22bf33d029
Carlos <1971975680@undc.edu.pe>
2023-12-09 04:59:22 -0500

    wa

--- assets/vista/proyectos/tabla-archivos.php
+++ assets/vista/proyectos/tabla-archivos.php
@@ -176,9 +176,15 @@ $query1 = mysqli_query($con, $sql1);
                         <i class="fas fa-angle-right fs-2 text-primary mx-1"></i> <a href="/../proyectos/tareas?idProyecto=<?php echo $idProyecto ?>">TAREAS</a>
                     </div>
                 </div>
-
+                <?php
+                        include('../config.php');
+                        $sqlArchivosS = ("SELECT * FROM proyectosTareas WHERE idProyecto = '$idProyecto';");
+                        $queryArchivosS = mysqli_query($con, $sqlArchivosS);
+                        if ($queryArchivosS) {
+                            $totalArchivosS = mysqli_num_rows($queryArchivosS); // Contamos el total de registros
+                        }?>
                 <div class="badge badge-lg badge-primary">
-                    <span id="kt_file_manager_items_counter">82 items</span>
+                    <span id="kt_file_manager_items_counter"><?php echo $totalArchivosS; ?> carpetas</span>
                 </div>
             </div>
 
@@ -238,6 +244,29 @@ $query1 = mysqli_query($con, $sql1);
                                 </thead>
 
                                 <tbody class="fw-semibold text-gray-600">
+                                <?php
+                                include('../config.php');
+                                $sqlLTareas = ("SELECT 
+                                    proyectosTareas.id, 
+                                    proyectosTareas.nombre,
+                                    proyectosTareas.fechaAdd,
+                                    MAX(proyectosDocumentos.fechaAdd) AS fechaMod,
+                                    proyectosDocumentos.documento,
+                                    proyectosDocumentos.extension,
+                                    proyectosDocumentos.link 
+                                FROM 
+                                    `proyectosTareas` 
+                                INNER JOIN 
+                                    proyectosDocumentos ON proyectosTareas.id = proyectosDocumentos.idProyectoTarea 
+                                WHERE 
+                                    proyectosTareas.idProyecto = '$idProyecto'
+                                GROUP BY 
+                                    proyectosTareas.id;");
+                                $queryLTareas = mysqli_query($con, $sqlLTareas);?>
+                                <?php while ($DataArchivo = mysqli_fetch_assoc($queryArchivos)) {    
+                                $fecha = $DataArchivo['fechaAdd'];
+                                setlocale(LC_TIME, 'es_ES'); // Establecer la configuración regional a español
+                                $fechaFormateada = strftime("%d de %B del %Y", strtotime($fecha));?>
                                     <tr>
                                         <td data-order="account">
                                             <div class="d-flex align-items-center">
@@ -252,67 +281,6 @@ $query1 = mysqli_query($con, $sql1);
                                         <td data-order="Invalid date">-</td>
                                         <td class="text-end" data-kt-filemanager-table="action_dropdown">
                                             <div class="d-flex justify-content-end">
-                                                <!--begin::Share link-->
-                                                <div class="ms-2" data-kt-filemanger-table="copy_link">
-                                                    <button type="button"
-                                                        class="btn btn-sm btn-icon btn-light btn-active-light-primary"
-                                                        data-kt-menu-trigger="click"
-                                                        data-kt-menu-placement="bottom-end">
-                                                        <i class="ki-duotone ki-fasten fs-5 m-0"><span
-                                                                class="path1"></span><span class="path2"></span></i>
-                                                    </button>
-                                                    <!--begin::Menu-->
-                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-300px"
-                                                        data-kt-menu="true">
-                                                        <!--begin::Card-->
-                                                        <div class="card card-flush">
-                                                            <div class="card-body p-5">
-                                                                <!--begin::Loader-->
-                                                                <div class="d-flex"
-                                                                    data-kt-filemanger-table="copy_link_generator">
-                                                                    <!--begin::Spinner-->
-                                                                    <div class="me-5" data-kt-indicator="on">
-                                                                        <span class="indicator-progress">
-                                                                            <span
-                                                                                class="spinner-border spinner-border-sm align-middle ms-2"></span>
-                                                                        </span>
-                                                                    </div>
-                                                                    <!--end::Spinner-->
-
-                                                                    <!--begin::Label-->
-                                                                    <div class="fs-6 text-gray-900">Generating Share
-                                                                        Link...</div>
-                                                                    <!--end::Label-->
-                                                                </div>
-                                                                <!--end::Loader-->
-
-                                                                <!--begin::Link-->
-                                                                <div class="d-flex flex-column text-start d-none"
-                                                                    data-kt-filemanger-table="copy_link_result">
-                                                                    <div class="d-flex mb-3">
-                                                                        <i
-                                                                            class="ki-duotone ki-check fs-2 text-success me-3"></i>
-                                                                        <div class="fs-6 text-gray-900">Share Link
-                                                                            Generated</div>
-                                                                    </div>
-                                                                    <input type="text"
-                                                                        class="form-control form-control-sm"
-                                                                        value="https://path/to/file/or/folder/">
-                                                                    <div class="text-muted fw-normal mt-2 fs-8 px-3">
-                                                                        Read only. <a
-                                                                            href="/metronic8/demo14/?page=apps/file-manager/settings/"
-                                                                            class="ms-2">Change permissions</a></div>
-                                                                </div>
-                                                                <!--end::Link-->
-                                                            </div>
-                                                        </div>
-                                                        <!--end::Card-->
-                                                    </div>
-                                                    <!--end::Menu-->
-                                                    <!--end::Share link-->
-                                                </div>
-
-                                                <!--begin::More-->
                                                 <div class="ms-2">
                                                     <button type="button"
                                                         class="btn btn-sm btn-icon btn-light btn-active-light-primary me-2"
@@ -322,36 +290,19 @@ $query1 = mysqli_query($con, $sql1);
                                                                 class="path1"></span><span class="path2"></span><span
                                                                 class="path3"></span><span class="path4"></span></i>
                                                     </button>
-                                                    <!--begin::Menu-->
                                                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
                                                         data-kt-menu="true">
-                                                        <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
                                                             <a href="/metronic8/demo14/?page=apps/file-manager/files"
                                                                 class="menu-link px-3">
                                                                 View
                                                             </a>
                                                         </div>
-                                                        <!--end::Menu item-->
-
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3"
-                                                                data-kt-filemanager-table="rename">
-                                                                Rename
-                                                            </a>
-                                                        </div>
-                                                        <!--end::Menu item-->
-
-                                                        <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
                                                             <a href="#" class="menu-link px-3">
                                                                 Download Folder
                                                             </a>
                                                         </div>
-                                                        <!--end::Menu item-->
-
-                                                        <!--begin::Menu item-->
                                                         <div class="menu-item px-3">
                                                             <a href="#" class="menu-link px-3"
                                                                 data-kt-filemanager-table-filter="move_row"
@@ -360,23 +311,13 @@ $query1 = mysqli_query($con, $sql1);
                                                                 Move to folder
                                                             </a>
                                                         </div>
-                                                        <!--end::Menu item-->
 
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link text-danger px-3"
-                                                                data-kt-filemanager-table-filter="delete_row">
-                                                                Delete
-                                                            </a>
-                                                        </div>
-                                                        <!--end::Menu item-->
                                                     </div>
-                                                    <!--end::Menu-->
-                                                    <!--end::More-->
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
+                                    <?php } ?>
                                 </tbody>
                             </table>
                         </div>
