8690610a647d457891217ee4d28d9593584a9ed7
Carlos <1971975680@undc.edu.pe>
2023-12-09 05:14:24 -0500

    wa

--- assets/vista/proyectos/tabla-archivos.php
+++ assets/vista/proyectos/tabla-archivos.php
@@ -303,28 +303,19 @@ $query1 = mysqli_query($con, $sql1);
                                                                 class="path1"></span><span class="path2"></span><span
                                                                 class="path3"></span><span class="path4"></span></i>
                                                     </button>
-                                                    <div class="menu menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
+                                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-150px py-4"
                                                         data-kt-menu="true">
                                                         <div class="menu-item px-3">
                                                             <a href="/metronic8/demo14/?page=apps/file-manager/files"
                                                                 class="menu-link px-3">
-                                                                View
+                                                                Ver
                                                             </a>
                                                         </div>
                                                         <div class="menu-item px-3">
                                                             <a href="#" class="menu-link px-3">
-                                                                Download Folder
+                                                                Descargar
                                                             </a>
                                                         </div>
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3"
-                                                                data-kt-filemanager-table-filter="move_row"
-                                                                data-bs-toggle="modal"
-                                                                data-bs-target="#kt_modal_move_to_folder">
-                                                                Move to folder
-                                                            </a>
-                                                        </div>
-
                                                     </div>
                                                 </div>
                                             </div>
