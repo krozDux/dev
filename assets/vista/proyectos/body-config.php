9907d4578946f92d9c18dd109ce24a95fc3f8f86
Carlos <1971975680@undc.edu.pe>
2023-12-09 23:58:50 -0500

    wa

---
+++ assets/vista/proyectos/body-config.php
@@ -0,0 +1,106 @@
+<?php  if ($session_rol != "invitado") {?>
+<?php include '../assets/controlador/recursos.php'?>
+<div class="d-flex flex-column flex-root">
+    <div class="page d-flex flex-row flex-column-fluid">
+        <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+            <div id="kt_header" class="header">
+                <div class="container-fluid d-flex flex-stack">
+                    <div class="d-flex align-items-center me-5">
+                        <div class="d-lg-none btn btn-icon btn-active-color-white w-30px h-30px ms-n2 me-3"
+                            id="kt_aside_toggle">
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <path
+                                        d="M21 7H3C2.4 7 2 6.6 2 6V4C2 3.4 2.4 3 3 3H21C21.6 3 22 3.4 22 4V6C22 6.6 21.6 7 21 7Z"
+                                        fill="currentColor" />
+                                    <path opacity="0.3"
+                                        d="M21 14H3C2.4 14 2 13.6 2 13V11C2 10.4 2.4 10 3 10H21C21.6 10 22 10.4 22 11V13C22 13.6 21.6 14 21 14ZM22 20V18C22 17.4 21.6 17 21 17H3C2.4 17 2 17.4 2 18V20C2 20.6 2.4 21 3 21H21C21.6 21 22 20.6 22 20Z"
+                                        fill="currentColor" />
+                                </svg>
+                            </span>
+                        </div>
+                        <a href="panel/index.php">
+                            <img alt="Logo" src="assets/media/logos/logo-2.svg" class="h-25px h-lg-30px" />
+                        </a>
+                    </div>
+                    <div class="d-flex align-items-center flex-shrink-0">
+                        <div class="d-flex align-items-center ms-1">
+                            <div class="btn btn-icon btn-color-white bg-hover-white bg-hover-opacity-10 w-30px h-30px h-40px w-40px"
+                                id="kt_activities_toggle">
+                                <span class="svg-icon svg-icon-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none">
+                                        <rect x="8" y="9" width="3" height="10" rx="1.5" fill="currentColor" />
+                                        <rect opacity="0.5" x="13" y="5" width="3" height="14" rx="1.5"
+                                            fill="currentColor" />
+                                        <rect x="18" y="11" width="3" height="8" rx="1.5" fill="currentColor" />
+                                        <rect x="3" y="13" width="3" height="6" rx="1.5" fill="currentColor" />
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <?php include_once '../assets/controlador/header.php'?>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex flex-column-fluid">
+                <?php include_once 'menu-recursos.php'?>
+                <div class="d-flex flex-column flex-column-fluid container-fluid">
+                    <div class="toolbar mb-2 mb-lg-2" id="kt_toolbar">
+                        <div class="page-title d-flex flex-column me-3 mb-1">
+                            <h1 class="d-flex text-dark fw-bolder my-1 fs-3">Recursos</h1>
+                        </div>
+                    </div>
+                    <?php include_once 'tabla-archivos.php'?>
+                    <div class="footer py-4 d-flex flex-column flex-md-row flex-stack" id="kt_footer">
+                        <?php include_once 'footer-proyectos.php'?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="kt_modal_new_target" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <div class="modal-content rounded">
+            <div class="modal-header pb-0 border-0 justify-content-end">
+                <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                    <span class="svg-icon svg-icon-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="modal fade" id="kt_modal_new_user" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <div class="modal-content rounded">
+            <div class="modal-header pb-0 border-0 justify-content-end">
+                <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+                    <span class="svg-icon svg-icon-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                fill="currentColor" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php } else{
+header("location: ../panel/index.php");
+} ?>
