1d430d55219f222022b31028315ac7bfa392ff5f
Carlos <1971975680@undc.edu.pe>
2023-12-10 00:23:55 -0500

    wa

--- assets/vista/proyectos/tabla-config.php
+++ assets/vista/proyectos/tabla-config.php
@@ -209,7 +209,7 @@ $query1 = mysqli_query($con, $sql1);
                         <div class="fs-6 fw-semibold mt-2 mb-3">Fechas del proyecto</div>
                     </div>
 
-                    <div class="col-xl-3 fv-row fv-plugins-icon-container">
+                    <div class="col-xl-6 fv-row fv-plugins-icon-container">
                         <label class="required fs-6 semibold mb-2">Fecha de inicio</label>
                         <div class="position-relative d-flex align-items-center">
                             <span class="svg-icon svg-icon-2 position-absolute mx-4">
@@ -230,8 +230,8 @@ $query1 = mysqli_query($con, $sql1);
                                 placeholder="Seleccionar fecha" name="fechaInicio" required />
                         </div>
                     </div>
-                    <div class="col-xl-3 fv-row fv-plugins-icon-container">
-                        <label class="required fs-6 semibold mb-2">Fecha de inicio</label>
+                    <div class="col-xl-6 fv-row fv-plugins-icon-container">
+                        <label class="required fs-6 semibold mb-2">Fecha límite</label>
                         <div class="position-relative d-flex align-items-center">
                             <span class="svg-icon svg-icon-2 position-absolute mx-4">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
