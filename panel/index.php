cebcf57c3416c0f8bb6ff12b663f8a758765a39a
Carlos <1971975680@undc.edu.pe>
2024-01-06 19:57:08 -0500

    GA

--- panel/index.php
+++ panel/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="es">
+<?php $titulo = "escritorio"; ?>
 <?php include_once '../assets/controlador/sesion.php'?>
-<?php include_once '../assets/vista/panel/head-panel.php'?>
-
+<?php include_once '../assets/vista/header.php'?>
 <body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed">
     <?php include_once '../assets/vista/panel/body-panel.php'?>
     <script src="assets/plugins/global/plugins.bundle.js"></script>
@@ -17,5 +17,4 @@
     <script src="assets/js/custom/utilities/modals/users-search.js"></script>+
     <script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
 </body>
-
 </html>
