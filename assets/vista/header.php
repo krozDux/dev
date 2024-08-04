a34c16d67f3857064e531222c27392343458fa17
Carlos <1971975680@undc.edu.pe>
2024-08-03 23:46:40 -0500

    WA

--- assets/vista/header.php
+++ assets/vista/header.php
@@ -1,14 +1,14 @@
 <head>
     <base href="../">
-    <title>SistemaWEB | <?php echo ucfirst($titulo); ?></title>
+    <title>TDA | <?php echo ucfirst($titulo); ?></title>
     <meta charset="utf-8" />
     <meta name="description" content="descripción" />
     <meta name="keywords" content="keywords" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:locale" content="es_PE" />
-    <meta property="og:title" content="SistemaWEB | <?php echo ucfirst($titulo); ?>" />
+    <meta property="og:title" content="TDA | <?php echo ucfirst($titulo); ?>" />
     <meta property="og:url" content="url" />
-    <meta property="og:site_name" content="SistemaWEB" />
+    <meta property="og:site_name" content="TDA" />
     <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
